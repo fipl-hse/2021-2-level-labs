4685b5509c2c3cee5307052428128501c1671e60
WhiteJaeger <logerk3@gmail.com>
2021-09-12 09:36:43 +0300

    add tests for 8

---
+++ lab_1/compare_profiles_advanced_test.py
@@ -0,0 +1,100 @@
+# pylint: skip-file
+"""
+Checks the first lab language comparison function
+"""
+
+import unittest
+from main import compare_profiles_advanced
+
+
+class CompareProfilesAdvancedTest(unittest.TestCase):
+    """
+    Tests profile comparison advanced  function
+    """
+
+    def test_compare_profiles_ideal(self):
+        """
+        Ideal scenario
+        """
+        en_profile = {'name': 'en',
+                      'freq': {'happy': 2, 'he': 1, 'man': 1, 'a': 2},
+                      'n_words': 3}
+
+        unk_profile = {'name': 'unk',
+                       'freq': {'ich': 3, 'weiß': 1, 'nicht': 1, 'machen': 1,
+                                'möchte': 1, 'vielleicht': 1, 'überlegen': 1, 'man': 1, 'a': 3},
+                       'n_words': 9}
+
+        expected = {'name': 'en',
+                    'common': ['a', 'man'],
+                    'score': 0.222,
+                    'max_length_word': 'man',
+                    'min_length_word': 'a',
+                    'average_token_length': 0,
+                    'sorted_common': ['a', 'man']}
+
+        actual = compare_profiles_advanced(unk_profile, en_profile, 2)
+        self.assertEqual(expected, actual)
+
+    def test_compare_profiles_ideal_deutsch(self):
+        """
+        Ideal scenario with deutsch
+        """
+        unk_profile = {'name': 'de',
+                       'freq': {'ich': 5, 'weiß': 1, 'nicht': 1,
+                                'staat': 2, 'wunderbar': 1, 'möchte': 1},
+                       'n_words': 6}
+
+        de_profile = {'name': 'de',
+                      'freq': {'ich': 3, 'weiß': 1, 'nicht': 1, 'machen': 1,
+                               'möchte': 1, 'vielleicht': 1, 'überlegen': 1, 'man': 1},
+                      'n_words': 8}
+
+        expected = {'name': 'de',
+                    'common': ['möchte', 'nicht', 'weiß', 'ich'],
+                    'score': 0.714,
+                    'max_length_word': 'möchte',
+                    'min_length_word': 'ich',
+                    'average_token_length': 0,
+                    'sorted_common': ['ich', 'möchte', 'nicht', 'weiß']}
+
+        actual = compare_profiles_advanced(unk_profile, de_profile, 4)
+        self.assertEqual(expected, actual)
+
+    def test_compare_profiles_bad_input(self):
+        """
+        Bad input scenario
+        """
+        unk_profile = [{'name': 'de',
+                        'freq': {'ich': 5, 'weiß': 1, 'nicht': 1,
+                                 'staat': 2, 'wunderbar': 1, 'möchte': 1},
+                        'n_words': 6}]
+
+        de_profile = {'name': 'de',
+                      'freq': {'ich': 3, 'weiß': 1, 'nicht': 1, 'machen': 1,
+                               'möchte': 1, 'vielleicht': 1, 'überlegen': 1, 'man': 1},
+                      'n_words': 8}
+
+        expected = None
+
+        actual = compare_profiles_advanced(unk_profile, de_profile, 4)
+        self.assertEqual(expected, actual)
+
+    def test_compare_profiles_bad_input_top_n(self):
+        """
+        Bad input scenario
+        """
+        unk_profile = {'name': 'de',
+                       'freq': {'ich': 5, 'weiß': 1, 'nicht': 1,
+                                'staat': 2, 'wunderbar': 1, 'möchte': 1},
+                       'n_words': 6}
+
+        de_profile = {'name': 'de',
+                      'freq': {'ich': 3, 'weiß': 1, 'nicht': 1, 'machen': 1,
+                               'möchte': 1, 'vielleicht': 1, 'überlegen': 1, 'man': 1},
+                      'n_words': 8}
+
+        expected = None
+
+        actual = compare_profiles_advanced(unk_profile, de_profile, [])
+        self.assertEqual(expected, actual)
--- lab_1/compare_profiles_test.py
+++ lab_1/compare_profiles_test.py
@@ -4,7 +4,7 @@ Checks the first lab language comparison function
 """
 
 import unittest
-from main import calculate_distance
+from main import compare_profiles
 
 
 class CompareProfilesTest(unittest.TestCase):
@@ -26,7 +26,7 @@ class CompareProfilesTest(unittest.TestCase):
                       'n_words': 8}
 
         expected = 0.25
-        actual = calculate_distance(en_profile, de_profile, 4)
+        actual = compare_profiles(en_profile, de_profile, 4)
         self.assertEqual(expected, actual)
 
     def test_compare_profiles_no_intersections_ideal(self):
@@ -43,7 +43,7 @@ class CompareProfilesTest(unittest.TestCase):
                       'n_words': 7}
 
         expected = 0.0
-        actual = calculate_distance(en_profile, de_profile, 4)
+        actual = compare_profiles(en_profile, de_profile, 4)
         self.assertEqual(expected, actual)
 
     def test_compare_profiles_identical(self):
@@ -59,7 +59,7 @@ class CompareProfilesTest(unittest.TestCase):
                           'n_words': 3}
 
         expected = 1.0
-        actual = calculate_distance(first_profile, second_profile, 2)
+        actual = compare_profiles(first_profile, second_profile, 2)
         self.assertEqual(expected, actual)
 
     def test_compare_profiles_bad_input(self):
@@ -74,7 +74,7 @@ class CompareProfilesTest(unittest.TestCase):
                       'n_words': 7}
 
         expected = None
-        actual = calculate_distance(en_profile, de_profile, 4)
+        actual = compare_profiles(en_profile, de_profile, 4)
         self.assertEqual(expected, actual)
 
     def test_compare_profiles_bad_input_top_n(self):
@@ -91,5 +91,5 @@ class CompareProfilesTest(unittest.TestCase):
                       'n_words': 7}
 
         expected = None
-        actual = calculate_distance(en_profile, de_profile, dict())
+        actual = compare_profiles(en_profile, de_profile, dict())
         self.assertEqual(expected, actual)
