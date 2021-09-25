a79c6817790303b39f8d48b41df8475b62c4736b
lenazagg <lenazagovorova@gmail.com>
2021-09-25 16:47:18 +0300

    5 done

--- lab_1/main.py
+++ lab_1/main.py
@@ -13,15 +13,15 @@ def tokenize(text: str):
     """
     pass
 
-    import re
+    import re # импортирую модуль re для последующей работы с регулярными выражениями
 
-    if type(text) != str:
+    if type(text) != str: # если тип переменной text не является строкой, то возвращаем None
         return None
-        text = text.lower()
-        text = re.sub(r'[^a-zäöüß ]', '', str(text))
-        tokens = text.split()
+    text = text.lower() # приводу текст к нижнему регистру с помощью функции .lower()
+    text = re.sub(r'[^a-zäöüß ]', '', str(text)) # с помощью sub ищем символы, кроме букв и заменяем на пробел
+    tokens = text.split() # с помощью split разбиваем строку на части
 
-        return tokens
+    return tokens
 
 
 def remove_stop_words(tokens: list, stop_words: list):
@@ -37,9 +37,9 @@ def remove_stop_words(tokens: list, stop_words: list):
         return None
     if type(stop_words) != list:
         return None
-    for token in tokens:
+    for token in tokens: # проходим по токенам в tokens
         if token in stop_words:
-            tokens.remove(token)
+            tokens.remove(token) # если токен - стоп-слово, то удаляем его
             return tokens
 
 
@@ -51,7 +51,7 @@ def calculate_frequencies(tokens: list):
         """
     pass
 
-    freq_dict = {}
+    freq_dict = {} # создаем частотный словарь
 
     if type(tokens) != list:
         return None
@@ -63,7 +63,7 @@ def calculate_frequencies(tokens: list):
             freq_dict[token] = 1
         else:
             freq_dict[token] += 1
-    print(freq_dict)
+
     return freq_dict
 
 def get_top_n_words(freq_dict: str, top_n: int):
@@ -80,15 +80,17 @@ def get_top_n_words(freq_dict: str, top_n: int):
     if type(top_n) != int:
         return None
 
-    freq_dict = list(freq_dict.items())
+    freq_dict = list(freq_dict.items()) # создаем список, с помощью items возвращаем k и v
 
-    freq_dict_sorted = sorted(freq_dict, key=lambda x: (-x[1], x[0]))
-    print('Words sorted by frequency:', freq_dict_sorted)
+    freq_dict_sorted = sorted(freq_dict, key=lambda x: (-x[1], x[0])) # key позволяет уточнить критерий,
+    # по которому происходит сортировка, x - это элемент нашего списка
+    # x[0] - нулевой элемент списка, это токен i
+    # x[1] - частота токена i
+    # поскольку по умолчанию сортировка идет по возрастанию, необходимо поставить "-"
+    # если же частоты равны, то используется x[0], который сортирует по алфавитному порядку
 
-    top_n = freq_dict_sorted[:5]
+    top_n = freq_dict_sorted[:top_n] # с помощью срезу выбираем топ-n по популярности слов
 
-    for k, v in top_n:
-        print('{}: {} time(s)'.format(k, v))
     return top_n
 
 
@@ -101,6 +103,22 @@ def create_language_profile(language: str, text: str, stop_words: list):
     :return: a dictionary with three keys – name, freq, n_words
     """
     pass
+
+    if type(language) != str:
+        return None
+    if type(text) != str:
+        return None
+    if type(stop_words) != list:
+        return None
+
+    language_profile = {} # создаем словарь - профиль языка
+    frequencies = calculate_frequencies(remove_stop_words(tokenize(text))) # получаем частотный словарь
+    n_words = len(frequencies) # получаем количество токенов в словаре
+    language_profile['freq'] = frequencies # ключ - freq, значение - частотный словарь
+    language_profile['name'] = language # ключ - name, значение - конкретный язык
+    language_profile['n_words'] = n_words # ключ - n_words, значение - количество токенов
+
+    return language_profile
 
 
 def compare_profiles(unknown_profile: dict, profile_to_compare: dict, top_n: int):
