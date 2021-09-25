366b4c5cec1878cff253e678f8109c7eb5ff5092
tanynova99 <balabolka2000@gmail.com>
2021-09-26 02:43:51 +0300

    update main.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -3,6 +3,7 @@ Language detection starter
 """
 
 import os
+from main import *
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_TEXTS_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'texts')
@@ -15,11 +16,23 @@ if __name__ == '__main__':
     with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'de.txt'), 'r', encoding='utf-8') as file_to_read:
         de_text = file_to_read.read()
 
+    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'la.txt'), 'r', encoding='utf-8') as \
+            file_to_read:
+        la_text = file_to_read.read()
+
     with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'unknown.txt'), 'r', encoding='utf-8') as \
             file_to_read:
         unknown_text = file_to_read.read()
 
+
+    english = create_language_profile("en", en_text, [])
+    german = create_language_profile("de", de_text, [])
+    unknown = create_language_profile("unk", unknown_text, [])
+    latin = create_language_profile("la", la_text, [])
+
+
     EXPECTED = 'en'
-    RESULT = ''
+    print(RESULT)
+
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
