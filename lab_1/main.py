8bef810a98f77722fb6c2dcc889f62f38d948af0
Kate <kate2002elisey@gmail.com>
2021-09-25 16:49:35 +0300

    improve 1st function

--- lab_1/main.py
+++ lab_1/main.py
@@ -8,13 +8,11 @@ import re
 def tokenize(text: str) -> list or None:
     if isinstance(text, str):
         text = text.lower()
-        text = text.replace("'", "")
-        text = text.replace("\n", " ")
-        text = re.sub(r"[#%!@&*><.]", "", text)
-        text = re.sub(r"[^a-zāūīōēȳüßöä]+", " ", text)
-        text = re.sub(r'\s+', ' ', text)
-        text = ' '.join(text.split())
-        tokens = text.split(" ")
+        string_with_tokens = ""
+        for i in text:
+            if i.isalpha() or i.isspace():
+                string_with_tokens += i
+        tokens = string_with_tokens.split()
         return tokens
     else:
         return None
@@ -156,21 +154,3 @@ def detect_language_advanced(unknown_profile: dict, profiles: list, languages: l
             return max_score
     else:
         None
-
-
-def load_profile(path_to_file: str) -> dict or None:
-    """
-    Loads a language profile
-    :param path_to_file: a path
-    :return: a dictionary with three keys – name, freq, n_words
-    """
-    pass
-
-
-def save_profile(profile: dict) -> int:
-    """
-    Saves a language profile
-    :param profile: a dictionary
-    :return: 0 if everything is ok, 1 if not
-    """
-    pass
