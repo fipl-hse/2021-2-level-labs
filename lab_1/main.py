edf528612d4adb87c6cbc43e03cf2370d04e7929
DashaKrinochkina <dasha060402@mail.ru>
2021-09-26 15:55:52 +0300

    style.

--- lab_1/main.py
+++ lab_1/main.py
@@ -142,7 +142,8 @@ def compare_profiles(unknown_profile: dict, profile_to_compare: dict, top_n: int
     return distance
 
 
-def detect_language(unknown_profile: dict, profile_1: dict, profile_2: dict, top_n: int) -> str or None:
+def detect_language(unknown_profile: dict, profile_1: dict,
+                    profile_2: dict, top_n: int) -> str or None:
     """
     Detects the language of an unknown profile
     :param unknown_profile: a dictionary
@@ -171,7 +172,8 @@ def detect_language(unknown_profile: dict, profile_1: dict, profile_2: dict, top
     return language
 
 
-def compare_profiles_advanced(unknown_profile: dict, profile_to_compare: dict, top_n: int) -> list or None:
+def compare_profiles_advanced(unknown_profile: dict, profile_to_compare: dict,
+                              top_n: int) -> list or None:
     """
     Compares profiles and calculates some advanced parameters
     :param unknown_profile: a dictionary
@@ -204,8 +206,8 @@ def compare_profiles_advanced(unknown_profile: dict, profile_to_compare: dict, t
     average_len = sum(tokens_len) / len(tokens)
 
     report = {'name': profile_to_compare['name'], 'common': top_n_common, 'score': score,
-              'max_length_word': max_len, 'min_length_word': min_len, 'average_token_length': average_len,
-              'sorted_common': sorted_common}
+              'max_length_word': max_len, 'min_length_word': min_len,
+              'average_token_length': average_len, 'sorted_common': sorted_common}
     return report
 
 
