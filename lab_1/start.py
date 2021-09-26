da6f8ff7f349f751885b0a8ba32818c446601916
lyutl <rajda02@gmail.com>
2021-09-26 18:46:45 +0300

    Start.py improvement

--- lab_1/start.py
+++ lab_1/start.py
@@ -23,17 +23,29 @@ if __name__ == '__main__':
             file_to_read:
         unknown_text = file_to_read.read()
 
-    de_profile = main.load_profile(os.path.join(PATH_TO_PROFILES, 'de.json'))
-    en_profile = main.load_profile(os.path.join(PATH_TO_PROFILES, 'en.json'))
-    la_profile = main.load_profile(os.path.join(PATH_TO_PROFILES, 'la.json'))
-    profiles = [de_profile, en_profile, la_profile]
-
+    top_n = 9
     unknown_profile = main.create_language_profile('unk', unknown_text, [])
-    comparison = main.compare_profiles_advanced(unknown_profile, en_profile, 8)
-    detection = main.detect_language_advanced(unknown_profile, profiles, [], 8)
 
-    print('Comparison result:', comparison)
-    print('Detection result:', detection)
+    de_profile_1 = main.load_profile(os.path.join(PATH_TO_PROFILES, 'de.json'))
+    en_profile_1 = main.load_profile(os.path.join(PATH_TO_PROFILES, 'en.json'))
+    la_profile_1 = main.load_profile(os.path.join(PATH_TO_PROFILES, 'la.json'))
+    profiles_1 = [de_profile_1, en_profile_1, la_profile_1]
+
+    detection_1 = main.detect_language_advanced(unknown_profile, profiles_1, [], top_n)
+    print('External detection result:', detection_1)
+
+    de_profile_2 = main.create_language_profile('de', de_text, [])
+    en_profile_2 = main.create_language_profile('en', en_text, [])
+    la_profile_2 = main.create_language_profile('la', la_text, [])
+    profiles_2 = [de_profile_2, en_profile_2, la_profile_2]
+
+    detection_2 = main.detect_language_advanced(unknown_profile, profiles_2, [], top_n)
+    print('Internal detection result:', detection_2)
+
+    if detection_1 == detection_2:
+        print('External result = internal result')
+    else:
+        print('Different results')
 
     main.save_profile(unknown_profile)
 
