230de8e8e6aa8a25b930f9c3f5d49ab4d6f9d6d8
KseniaEremeeva <irtn2002@yandex.ru>
2021-09-27 21:38:49 +0500

    Корректировки в коде

--- lab_1/start.py
+++ lab_1/start.py
@@ -4,8 +4,6 @@ Language detection starter
 
 import os
 
-
-
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_TEXTS_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'texts')
 
@@ -23,52 +21,6 @@ if __name__ == '__main__':
 
     EXPECTED = 'en'
     RESULT = ''
-
-    from main import tokenize, remove_stop_words, calculate_frequencies, get_top_n_words, \
-    create_language_profile, compare_profiles, detect_language, tokens_en, stop_words_en, \
-    tokens_de, stop_words_de, tokens_unknown, stop_words_unknown, freq_dict_en, top_n_en, \
-    freq_dict_de, top_n_de, freq_dict_unknown, top_n_unknown, language_en, language_de, \
-    language_unknown, unknown_profile, en_profile, de_profile, en_stop_words, de_stop_words, \
-    unknown_stop_words
-
-
-
-    print(tokenize(en_text))
-    print(tokenize(de_text))
-    print(tokenize(unknown_text))
-
-    print()
-
-    print(remove_stop_words(tokens_en, stop_words_en))
-    print(remove_stop_words(tokens_de, stop_words_de))
-    print(remove_stop_words(tokens_unknown, stop_words_unknown))
-
-    print()
-
-    print(calculate_frequencies(tokens_en))
-    print(calculate_frequencies(tokens_de))
-    print(calculate_frequencies(tokens_unknown))
-
-    print()
-
-    print(get_top_n_words(freq_dict_en, top_n_en))
-    print(get_top_n_words(freq_dict_de, top_n_de))
-    print(get_top_n_words(freq_dict_unknown, top_n_unknown))
-
-    print()
-
-    print(create_language_profile(language_en, en_text, en_stop_words))
-    print(create_language_profile(language_de, de_text, de_stop_words))
-    print(create_language_profile(language_unknown, unknown_text, unknown_stop_words))
-
-    print()
-
-    print(compare_profiles(unknown_profile, en_profile, top_n_en))
-    print(compare_profiles(unknown_profile, de_profile, top_n_de))
-
-    print()
-
-    print(detect_language(unknown_profile, en_profile, de_profile, top_n_unknown))
 
     #DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
