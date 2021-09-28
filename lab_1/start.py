f79e4e8c53b53e7b9bce2273f130da90f02c6343
veronicamanukyan <vermanuk@mail.ru>
2021-09-28 21:24:02 +0300

    Add realisation

--- lab_1/start.py
+++ lab_1/start.py
@@ -4,6 +4,8 @@ Language detection starter
 
 import os
 
+from lab_1.main import create_language_profile, detect_language
+
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_TEXTS_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'texts')
 
@@ -18,8 +20,11 @@ if __name__ == '__main__':
     with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'unknown.txt'), 'r', encoding='utf-8') as \
             file_to_read:
         unknown_text = file_to_read.read()
+    unknown_profile = create_language_profile('unknown', unknown_text, [])
+    profile_en = create_language_profile("en", en_text, [])
+    profile_de = create_language_profile("de", de_text, [])
 
     EXPECTED = 'en'
-    RESULT = ''
+    RESULT = detect_language(unknown_profile, profile_en,profile_de,5)
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
