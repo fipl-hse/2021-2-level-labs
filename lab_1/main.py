54b1d1cf372835be90d6bdd51678d3c048152e68
tanynova99 <balabolka2000@gmail.com>
2021-09-28 22:27:45 +0300

    try statements removed, ifs implemented
    hard to understand places are either rewritten or redone
    
    long statements shortened

--- lab_1/main.py
+++ lab_1/main.py
@@ -40,7 +40,6 @@ def remove_stop_words(tokens: list, stop_words: list) -> list or None:
         return tokens_update
 
 
-
 def calculate_frequencies(tokens: list) -> dict or None:
     """
     Calculates frequencies of given tokens
@@ -74,14 +73,15 @@ def get_top_n_words(freq_dict: dict, top_n: int) -> list or None:
         return None
     else:
         most_common = []
-        chart = sorted(list(freq_dict.values()), reverse=True)
-        chart = chart[:top_n]
-        freq_dict_temp = dict(freq_dict)
-        for i in chart:
-            word = list(freq_dict_temp.keys())[list(freq_dict_temp.values()).index(i)]
-            most_common.append(word)
-            freq_dict_temp.pop(word)
-        return most_common
+        sorted_freqs = sorted(freq_dict.values(), reverse=True)
+        for freq in sorted_freqs:
+            for k, v in freq_dict.items():
+                if v == freq:
+                    most_common.append(k)
+
+    most_common = list(dict.fromkeys(most_common))
+
+    return most_common[:top_n]
 
 
 def create_language_profile(language: str, text: str, stop_words: list) -> dict or None:
@@ -92,20 +92,13 @@ def create_language_profile(language: str, text: str, stop_words: list) -> dict 
     :param stop_words: a list of stop words
     :return: a dictionary with three keys – name, freq, n_words
     """
-    try:
-        text_tmp = text
-        text_tmp = tokenize(text_tmp)
-        text_tmp = remove_stop_words(text_tmp, stop_words)
-        text_tmp = calculate_frequencies(text_tmp)
-        if text_tmp is None:
-            return None
-        else:
-            if language.isalpha():
-                lan_profile = {"name": language, "freq": text_tmp}
-                lan_profile["n_words"] = len(lan_profile["freq"])
-            return lan_profile
-    except AttributeError:
+    if not isinstance(language, str) or not isinstance(text, str) or not isinstance(stop_words, list):
         return None
+    else:
+        text_tmp = calculate_frequencies(remove_stop_words(tokenize(text), stop_words))
+        lan_profile = {"name": language, "freq": text_tmp, "n_words": len(text_tmp)}
+
+    return lan_profile
 
 
 def compare_profiles(unknown_profile: dict, profile_to_compare: dict, top_n: int) -> float or None:
@@ -116,7 +109,11 @@ def compare_profiles(unknown_profile: dict, profile_to_compare: dict, top_n: int
     :param top_n: a number of the most common words
     :return: the distance
     """
-    try:
+    if not isinstance(profile_to_compare, dict) or not isinstance(unknown_profile, dict):
+        return None
+    elif not isinstance(top_n, int):
+        return None
+    else:
         n_unknown = get_top_n_words(unknown_profile["freq"], top_n)
         n_compare = get_top_n_words(profile_to_compare["freq"], top_n)
         cross = 0
@@ -124,9 +121,7 @@ def compare_profiles(unknown_profile: dict, profile_to_compare: dict, top_n: int
             if token in n_unknown:
                 cross += 1
 
-        return round(cross / top_n, 2)
-    except TypeError:
-        return None
+    return round(cross / top_n, 2)
 
 
 def detect_language(unknown_profile: dict, profile_1: dict,
@@ -139,7 +134,12 @@ def detect_language(unknown_profile: dict, profile_1: dict,
     :param top_n: a number of the most common words
     :return: a language
     """
-    try:
+    # the check are divided in order to avoid long lines
+    if not isinstance(unknown_profile, dict) or not isinstance(profile_1, dict):
+        return None
+    elif not isinstance(profile_2, dict) or not isinstance(top_n, int):
+        return None
+    else:
         p1_cross = compare_profiles(unknown_profile, profile_1, top_n)
         p2_cross = compare_profiles(unknown_profile, profile_2, top_n)
 
@@ -147,11 +147,8 @@ def detect_language(unknown_profile: dict, profile_1: dict,
             return profile_1["name"]
         if p2_cross > p1_cross:
             return profile_2["name"]
-        alphabetical = sorted(list[profile_1["name"], profile_2["name"]])
-        return alphabetical[0]
 
-    except TypeError:
-        return None
+    return sorted(list[profile_1["name"], profile_2["name"]])[0]
 
 
 def compare_profiles_advanced(unknown_profile: dict, profile_to_compare: dict,
@@ -164,36 +161,41 @@ def compare_profiles_advanced(unknown_profile: dict, profile_to_compare: dict,
     :return: a dictionary with 7 keys – name, score, common, sorted_common, max_length_word,
     min_length_word, average_token_length
     """
-    stats = {}
-    try:
+    if not isinstance(unknown_profile, dict) or not isinstance(profile_to_compare, dict):
+        return None
+    elif not isinstance(top_n, int):
+        return None
+    else:
+        stats = {}
         top_unknown = get_top_n_words(unknown_profile["freq"], top_n)
         top_compare = get_top_n_words(profile_to_compare["freq"], top_n)
-        stats["name"] = profile_to_compare["name"]
 
         cross = 0
-        stats["common"] = []
+        common = []
         for token in top_compare:
             if token in top_unknown:
                 cross += 1
-                stats["common"].append(token)
+                common.append(token)
 
-        stats["score"] = round(cross / top_n, 2)
-
-        stats["sorted_common"] = sorted(stats["common"])
         tokens_length = 0
         max_l = len(list(profile_to_compare["freq"].keys())[0])
         min_l = max_l
         for word in profile_to_compare["freq"]:
             if len(word) >= max_l:
-                stats["max_length_word"] = word
+                max_w = word
             if len(word) <= min_l:
-                stats["min_length_word"] = word
+                min_w = word
             tokens_length += len(word)
         tokens_number = len(profile_to_compare["freq"])
-        stats["average_token_length"] = tokens_length / tokens_number
-        return stats
-    except TypeError:
-        return None
+
+        stats = {"name": profile_to_compare["name"],
+                 "score": round(cross / top_n, 2),
+                 "common": common,
+                 "sorted_common": sorted(common),
+                 "min_length_word": min_w,
+                 "max_length_word": max_w,
+                 "average_token_length": tokens_length / tokens_number}
+    return stats
 
 
 def detect_language_advanced(unknown_profile: dict, profiles: list,
@@ -206,34 +208,43 @@ def detect_language_advanced(unknown_profile: dict, profiles: list,
     :param top_n: a number of the most common words
     :return: a language
     """
-    try:
+    # obvious type checks
+    if not isinstance(unknown_profile, dict) or not isinstance(profiles, list):
+        return None
+    else:
+        # checks given languages - if none then all profiles are used
         if not languages:
             languages = profiles.copy()
+        # counter will go through profiles
         count = 0
+        # best score and current score are created; for best score also name is taken
         score_best = 0
-        profile = profiles[count]
-        score_best_name = profile["name"]
+        score_best_name = profiles[count]["name"]
         score_current = 0
+        # helps with non existent language bad input
         check = True
         for language in languages:
+            # the current profile is taking which will be compared
+            profile = profiles[count]
+            # prevents errors when only one language is needed
             if isinstance(language, dict):
                 language = language["name"]
+            # current score comparison with the current profile is created
+            # if this if wasn't entered then the language isn't in profiles => None returned
             if language == profile["name"]:
                 score_current = compare_profiles_advanced(unknown_profile, profile, top_n)["score"]
                 check = False
+            # updates best score and name if necessary
             if score_current > score_best:
                 score_best_name = language
                 score_best = score_current
+            # if score is the same as the best - the names are alphabetically compared
             elif score_current == score_best:
                 compare = sorted([score_best_name, language])
                 score_best_name = compare[0]
             count += 1
-            try:
-                profile = profiles[count]
-            except IndexError:
-                pass
+
         if check:
             return None
+
         return score_best_name
-    except (UnboundLocalError, TypeError):
-        return None
