cccfe8f86308122ad5f0db2e2c02b3a049192c31
123 <a123@MacBook-Pro-123.local>
2021-09-28 19:07:29 +0300

    made start file

--- lab_1/start.py
+++ lab_1/start.py
@@ -3,7 +3,7 @@ Language detection starter
 """
 
 import os
-
+from main import create_language_profile, detect_language_advanced
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_TEXTS_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'texts')
 
@@ -18,7 +18,12 @@ if __name__ == '__main__':
     with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'unknown.txt'), 'r', encoding='utf-8') as \
             file_to_read:
         unknown_text = file_to_read.read()
-
+    unknown_profile = create_language_profile('unknown', unknown_text, [])
+    en_profile = create_language_profile('en', en_text, [])
+    de_profile = create_language_profile('de', de_text, [])
+    profiles = [en_profile, de_profile]
+    result = detect_language_advanced(unknown_profile, profiles, ['en', 'de'], 3)
+    print(result)
     EXPECTED = 'en'
     RESULT = ''
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
