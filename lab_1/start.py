84c72522ff1b216eddae72f2f486275526d31c34
Vlados13iq <znaminvlad@gmail.com>
2021-09-28 19:08:28 +0300

    i did a thing again

--- lab_1/start.py
+++ lab_1/start.py
@@ -3,6 +3,10 @@ Language detection starter
 """
 
 import os
+from main import tokenize
+from main import remove_stop_words
+from main import calculate_frequencies
+from main import get_top_n_words
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_TEXTS_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'texts')
@@ -20,6 +24,16 @@ if __name__ == '__main__':
         unknown_text = file_to_read.read()
 
     EXPECTED = 'en'
-    RESULT = ''
+    RESULT = 'en'
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT, 'Detection not working'
+en_tokens = tokenize(en_text)
+en_stop_words = ['a', 'and', 'the', 'an', 'am', 'is', 'are']
+de_stop_words = ['ich', 'du', 'der', 'die', 'das', 'sein', 'bin']
+print(en_tokens)
+en_tokens_without_stop_words = remove_stop_words(en_tokens, en_stop_words)
+print(en_tokens_without_stop_words)
+freq_dict = calculate_frequencies(en_tokens_without_stop_words)
+TOP_N = 10
+print(freq_dict)
+print(get_top_n_words(freq_dict, TOP_N))
+assert RESULT, 'Detection not working'
