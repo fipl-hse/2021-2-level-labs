ec991dc095638da4ce02906d3d634e20f49c3284
KarinaZakirova <90447262+KarinaZakirova@users.noreply.github.com>
2021-09-29 18:11:39 +0300

    I have done this lab I hope 2.1

--- lab_1/main.py
+++ lab_1/main.py
@@ -17,7 +17,7 @@ def tokenize(text: str) -> list or None:
         return None
     invaluable_trash = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+',
                         '=', '{', '[', ']', '}', '|', '\\', ':', ';', '"', "'", '<', ',', '>',
-                        '.', '?', '/', '\t', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
+                        '.', '?', '/', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
     text = text.lower()
     for symbols in invaluable_trash:
         text = text.replace(symbols, '')
