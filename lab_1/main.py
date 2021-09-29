89f7bc3b1713f4a9c3b4aa8fbd3c44bfd73beab1
titovakaterina <90442736+titovakaterina@users.noreply.github.com>
2021-09-30 02:16:39 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -13,6 +13,17 @@ def tokenize(text: str) -> list or None:
     """
     return true
 
+def tokenize():
+    s = input('Введите строку: ')
+    import re
+    s_1 = re.sub(r'[^\w\s]', '', s)
+    s_2 = re.sub(r'[\d]', '', s_1)
+    a = s_2.split(' ')
+    a = [item.lower() for item in a]
+    print (a)
+
+tokenize ()
+
 
 def remove_stop_words(tokens: list, stop_words: list) -> list or None:
     """
@@ -23,6 +34,35 @@ def remove_stop_words(tokens: list, stop_words: list) -> list or None:
     """
     pass
 
+def remove_stop_words():
+    a = """0123456789=+'";:?/\|><.,@#№$%^&*()-_~`"""
+    oth = list(a)
+    tokens = list(input('Введите список токенов: ').split())
+    swords = list(input('Введите список стоп-слов: ').split())
+    n_tokens = []
+
+    for x in swords:
+        if x in oth:
+            print (tokens)
+
+    for i in tokens:
+        if i in oth:
+            return None
+
+    if isinstance(tokens, list) and isinstance(swords, list):
+        if all(isinstance(s, str) for s in tokens):
+             for i in tokens:
+                 if i not in swords:
+                     n_tokens.append(i)
+        else:
+            return None
+    else:
+        return None
+
+    print(n_tokens)
+
+remove_stop_words()
+
 
 def calculate_frequencies(tokens: list) -> dict or None:
     """
@@ -31,6 +71,29 @@ def calculate_frequencies(tokens: list) -> dict or None:
     :return: a dictionary with frequencies
     """
     pass
+
+def calculate_frequencies():
+    a = """0123456789=+'";:?/\|><.,@#№$%^&*()-_~`"""
+    oth = list(a)
+    tokens = list(input('Введите список токенов: ').split())
+
+    for i in tokens:
+        if i in oth:
+            return None
+
+    Dict = {}
+    for key in tokens:
+        key = key.lower()
+        if key in Dict:
+            value = Dict[key]
+            Dict[key] = value + 1
+        else:
+            Dict[key] = 1
+
+    fin = sorted(Dict, key=lambda x: int(Dict[x]), reverse=True)
+    print(fin)
+
+calculate_frequencies()
 
 
 def get_top_n_words(freq_dict: dict, top_n: int) -> list or None:
@@ -41,6 +104,23 @@ def get_top_n_words(freq_dict: dict, top_n: int) -> list or None:
     :return: a list of the most common words
     """
     pass
+
+def  get_top_n_words():
+    num = int(input('Число слов в топе: '))
+    freq_dict = {}
+    while True:
+        print('Введите токен (нажмите enter после ввода крайнего токена): ')
+        k = input()
+        if k == '':
+            break
+        print('Ведите частотность: ')
+        v = input()
+        freq_dict[k] = v
+    dictList = []
+    for key in freq_dict.keys():
+        dictList.append(key)
+    print(dictList[:num])
+get_top_n_words()
 
 
 def create_language_profile(language: str, text: str, stop_words: list) -> dict or None:
