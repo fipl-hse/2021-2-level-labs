7b958c6335896bc79bf06fe3b586d4ddc8f392bc
ArturIvanov228 <90447341+ArturIvanov228@users.noreply.github.com>
2021-09-29 15:22:12 +0300

    Update main.py

--- lab_1/main.py
+++ lab_1/main.py
@@ -12,10 +12,13 @@ def tokenize(text: str) -> list or None:
     :return: a list of lower-cased tokens without punctuation
     """
 
-    if isinstance(text, str):  # text является экземпляром классом str
+    if not (isinstance(text, str)):  # text не является экземпляром классом str
+        return None
+    else:
         text = text.lower()  # перевод строки в нижний регистр
         for char in text:
-            if char != ' ' and not (char.isalpha()):  # проверка то,что символ не пробел и не является буквой
+            if not(char.isspace()) and not (char.isalpha()):  # проверка то,что символ символ не пробел, не \t, \n,
+                #  и тд и не является буквой
                 text = text.replace(char, "")  # удаление этого символа
 
         tokens = text.split()
@@ -33,7 +36,9 @@ def remove_stop_words(tokens: list, stop_words: list):
     :param stop_words.words(language): a list of stop words
     :return: a list of tokens without stop words
     """
-    if isinstance(tokens, list) and isinstance(stop_words, list):
+    if not (isinstance(tokens, list) and isinstance(stop_words, list)):
+        return None
+    else:
         if tokens:  # проверка на то что список tokens не пустой
             for j in range(len(tokens)):  # иттерация по индексам элементов списка tokens
                 if tokens[j] in stop_words:  # проверка является ли элемент стоп словом
@@ -44,8 +49,6 @@ def remove_stop_words(tokens: list, stop_words: list):
             return tokens
         else:
             return None
-    else:
-        return None
 
 
 def calculate_frequencies(tokens: list) -> dict or None:
@@ -53,8 +56,10 @@ def calculate_frequencies(tokens: list) -> dict or None:
       Calculates frequencies of given tokens
       :param tokens: a list of tokens
       :return: a dictionary with frequencies
-      """
-    if isinstance(tokens, list):
+    """
+    if not (isinstance(tokens, list)):
+        return None
+    else:
         dict_frequencies = {}
         for token in tokens:  # иттерация по элементам списка tokens
             if isinstance(token, str):  # проверка является ли token строкой
@@ -66,8 +71,6 @@ def calculate_frequencies(tokens: list) -> dict or None:
             return dict_frequencies
         else:
             return None
-    else:
-        return None
 
 
 def get_top_n_words(freq_dict: dict, top_n: int) -> list or None:
@@ -77,12 +80,13 @@ def get_top_n_words(freq_dict: dict, top_n: int) -> list or None:
     :param top_n: a number of the most common words
     :return: a list of the most common words
     """
-    if isinstance(freq_dict, dict) and isinstance(top_n, int):
+    if not (isinstance(freq_dict, dict) and isinstance(top_n, int)):
+        return None
+    else:
         # сортировка словаря в обратном порядке, по второму элементу
         freq_dict = dict(sorted(freq_dict.items(), reverse=True, key=lambda value: value[1]))
         most_common_words = list(
             freq_dict)  # превращение словаря в список, который будет состоять только из ключей словаря
         most_common_words = most_common_words[:top_n]  # выборка первых top_n элементов из списка
         return most_common_words
-    else:
-        return None
+
