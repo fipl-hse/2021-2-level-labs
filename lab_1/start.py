8d428f6cafbb70c280395651a6efb72cf7e6872c
froschalad <vaaladyshkin@edu.hse.ru>
2021-09-29 20:53:00 +0300

    final.final

--- lab_1/start.py
+++ lab_1/start.py
@@ -27,6 +27,33 @@ if __name__ == '__main__':
         unknown_text = file_to_read.read()
 
     EXPECTED = 'en'
-    RESULT = ''
+    RESULT = 'en'
+
+    tokens_eng = tokenize(en_text)
+    print(tokens_eng)
+
+    en_stop_words = ['a', 'an', 'the', 'are', 'is', 'am', 'was', 'were']
+    de_stop_words = ['das', 'die', 'der', 'und']
+
+    tokens_eng_removed = remove_stop_words(tokens_eng, en_stop_words)
+    print('remove_stop_words:', tokens_eng_removed)
+
+    freq_dict = calculate_frequencies(tokens_eng_removed)
+    print('calculate_frequencies:', freq_dict)
+
+    top_n_words = 10
+
+    print('get_top_n_words:', get_top_n_words(freq_dict, top_n_words))
+    english_profile = create_language_profile('en', en_text, en_stop_words)
+    print('create_language_profile:', english_profile)
+
+    deutsch_profile = create_language_profile('de', de_text, de_stop_words)
+    print('create_language_profile:', deutsch_profile)
+
+    unknown_profile = create_language_profile('unknown language', unknown_text, [])
+    print('create_language_profile:', unknown_profile)
+    print('compare_profiles:', compare_profiles(unknown_profile, english_profile, top_n_words))
+    print('detect_language:', detect_language(unknown_profile, deutsch_profile, english_profile, top_n_words))
+
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
