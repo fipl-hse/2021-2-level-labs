b5f71679974c5e93f8d98f0a179561a605da2554
Gorbacheva Sasha <90442800+GorbachevaSasha@users.noreply.github.com>
2021-09-30 23:35:33 +0300

    Update start.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -3,9 +3,11 @@ Language detection starter
 """
 
 import os
+import main
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_TEXTS_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'texts')
+PATH_TO_PROFILE_FOLDER = os.path.join(PATH_TO_LAB_FOLDER,'profiles')
 
 if __name__ == '__main__':
 
@@ -15,11 +17,16 @@ if __name__ == '__main__':
     with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'de.txt'), 'r', encoding='utf-8') as file_to_read:
         de_text = file_to_read.read()
 
+    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'la.txt'), 'r', encoding='utf-8') as file_to_read:
+        la_text = file_to_read.read()
+
     with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'unknown.txt'), 'r', encoding='utf-8') as \
             file_to_read:
         unknown_text = file_to_read.read()
 
+    en_profile = main.create_language_profile('en', en_text, [])
+    de_profile = main.create_language_profile('de', de_text, [])
+    la_profile = main.create_language_profile('la', la_text, [])
+    unknown_profile = main.create_language_profile('unk', unknown_text, [])
+
     EXPECTED = 'en'
-    RESULT = ''
-    # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT, 'Detection not working'
