b433c1fc59880a252a440a1ffead7a08308f5f30
KseniaEremeeva <irtn2002@yandex.ru>
2021-10-02 16:02:52 +0500

    Исправлен некорректныйвывод данных

--- lab_1/start.py
+++ lab_1/start.py
@@ -24,85 +24,15 @@ if __name__ == '__main__':
 
     import main
 
-
-    print(main.tokenize(en_text))
-    print(main.tokenize(de_text))
-    print(main.tokenize(unknown_text))
-
-    print()
-
-
-    tokens_en = main.tokenize(en_text)
-    tokens_de = main.tokenize(de_text)
-    tokens_unknown = main.tokenize(unknown_text)
-
     stop_words_en = ['a', 'an', 'the', 'and']
     stop_words_de = ['der', 'das', 'die', 'ein', 'eine']
-    stop_words_unknown = []
 
-    print(main.remove_stop_words(tokens_en, stop_words_en))
-    print(main.remove_stop_words(tokens_de, stop_words_de))
-    print(main.remove_stop_words(tokens_unknown, stop_words_unknown))
+    en_profile = main.create_language_profile('en', en_text, stop_words_en)
+    de_profile = main.create_language_profile('de', de_text, stop_words_de)
+    unknown_profile = main.create_language_profile('unknown language', unknown_text, [])
 
-    print()
-
-
-    freq_dict_en = main.calculate_frequencies(tokens_en)
-    freq_dict_de = main.calculate_frequencies(tokens_de)
-    freq_dict_unknown = main.calculate_frequencies(tokens_unknown)
-
-    print(main.calculate_frequencies(tokens_en))
-    print(main.calculate_frequencies(tokens_de))
-    print(main.calculate_frequencies(tokens_unknown))
-
-    print()
-
-
-    new_dicts_en = {v: k for k, v in freq_dict_en.items()}
-    new_dicts_de = {v: k for k, v in freq_dict_de.items()}
-    new_dicts_unknown = {v: k for k, v in freq_dict_unknown.items()}
-
-    top_n_en = len(new_dicts_en)
-    top_n_de = len(new_dicts_de)
-    top_n_unknown = len(new_dicts_unknown)
-
-    print(main.get_top_n_words(freq_dict_en, top_n_en))
-    print(main.get_top_n_words(freq_dict_de, top_n_de))
-    print(main.get_top_n_words(freq_dict_unknown, top_n_unknown))
-
-    print()
-
-
-    en_stop_words = main.remove_stop_words(tokens_en, stop_words_en)
-    de_stop_words = main.remove_stop_words(tokens_de, stop_words_de)
-    unknown_stop_words = main.remove_stop_words(tokens_unknown, stop_words_unknown)
-
-    freq_dict_en = main.calculate_frequencies(main.remove_stop_words(main.tokenize(en_text), en_stop_words))
-    freq_dict_de = main.calculate_frequencies(main.remove_stop_words(main.tokenize(de_text), de_stop_words))
-    freq_dict_unknown = main.calculate_frequencies(main.remove_stop_words(main.tokenize(unknown_text), unknown_stop_words))
-
-
-    print(main.create_language_profile('en', en_text, en_stop_words))
-    print(main.create_language_profile('de', de_text, de_stop_words))
-    print(main.create_language_profile('unknown', unknown_text, unknown_stop_words))
-
-    print()
-
-
-    en_profile = main.create_language_profile('en', en_text, en_stop_words)
-    de_profile = main.create_language_profile('de', de_text, de_stop_words)
-    unknown_profile = main.create_language_profile('unknown', unknown_text, unknown_stop_words)
-
-    print(main.compare_profiles(unknown_profile, en_profile, top_n_en))
-    print(main.compare_profiles(unknown_profile, de_profile, top_n_de))
-
-    print()
-
-    top_n_en = len(main.get_top_n_words(freq_dict_en, top_n_en))
-    top_n_de = len(main.get_top_n_words(freq_dict_de, top_n_de))
-    top_n_unknown = len(main.get_top_n_words(freq_dict_unknown, top_n_unknown))
-
-    print(main.detect_language(unknown_profile, en_profile, de_profile, top_n_unknown))
+    print(main.compare_profiles(unknown_profile, en_profile, 10))
+    print(main.detect_language(unknown_profile, de_profile, en_profile, 10))
 
 
     #DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
