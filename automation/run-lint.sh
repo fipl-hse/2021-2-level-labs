7a37623360af862b14c96ac0e912e0706b491234
WhiteJaeger <logerk3@gmail.com>
2021-10-04 17:46:32 +0300

    use rc file

--- automation/run-lint.sh
+++ automation/run-lint.sh
@@ -11,6 +11,6 @@ for lab in $LABS; do
 
   TARGET_SCORE=$(cat lab_"${lab}"/target_score.txt)
 
-	lint_output=$(python3 -m pylint lab_"${lab}")
+	lint_output=$(python3 -m pylint --rcfile=automation/.pylintrc lab_"${lab}")
   python3 automation/check_lint.py --lint-output "$lint_output" --target-score $TARGET_SCORE
 done
