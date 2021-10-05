14e8114725a58227674c604fab4e89beccc7a936
WhiteJaeger <logerk3@gmail.com>
2021-10-05 22:17:40 +0300

    use lab_1 from main repository

--- lab_1/start.py
+++ lab_1/start.py
@@ -10,6 +10,7 @@ PATH_TO_TEXTS_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'texts')
 PATH_TO_PROFILES_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'profiles')
 
 if __name__ == '__main__':
+
     with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'en.txt'), 'r', encoding='utf-8') as file_to_read:
         en_text = file_to_read.read()
 
@@ -22,15 +23,6 @@ if __name__ == '__main__':
     with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'unknown.txt'), 'r', encoding='utf-8') as \
             file_to_read:
         unknown_text = file_to_read.read()
-
-    lang_unk = main.create_language_profile('unk', unknown_text, [])
-    lang_en = main.create_language_profile('en', en_text, [])
-    lang_de = main.create_language_profile('de', de_text, [])
-    lang_la = main.create_language_profile('la', la_text, [])
-
-    TOP_N = 4
-
-    langs = [lang_en, lang_de, lang_la]
 
     EXPECTED = 'en'
     RESULT = ''
