6e0b3044fd7d43fcd863d5d03174a95a5329dd4d
WhiteJaeger <logerk3@gmail.com>
2021-10-05 17:40:37 +0300

    use lab_1 from main repository

--- lab_1/start.py
+++ lab_1/start.py
@@ -24,14 +24,6 @@ if __name__ == '__main__':
             file_to_read:
         unknown_text = file_to_read.read()
 
-    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'la.txt'), 'r', encoding='utf-8') as \
-            file_to_read:
-        la_text = file_to_read.read()
-
-    en_profile = create_language_profile('en', en_text, [])
-    de_profile = create_language_profile('de', de_text, [])
-    unknown_profile = create_language_profile('unknown', unknown_text,[])
-    la_profile = create_language_profile('la', en_text, [])
     EXPECTED = 'en'
     RESULT = ''
     TOP_N = 7
