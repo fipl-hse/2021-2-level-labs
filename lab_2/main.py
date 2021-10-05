650ad2091d39910662aa60bd341472534d9ac932
AcipenserSturio <AcipenserSturio@users.noreply.github.com>
2021-10-06 01:32:42 +0500

    minor fixes

--- lab_2/main.py
+++ lab_2/main.py
@@ -71,7 +71,7 @@ def get_text_vector(original_text: list, language_profiles: dict) -> list or Non
         return None
     max_scores = {word: 0 for word in features}
     for profile in language_profiles.values():
-        for word, score in profile:
+        for word, score in profile.items():
             if score > max_scores[word]:
                 max_scores[word] = score
     return [max_scores[word] for word in features]
