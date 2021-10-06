5cd36b45a24c197d48c33eec48ed6c189b5c8913
dmitry-uraev <uraev.dima@yandex.ru>
2021-10-06 11:41:20 +0300

    update

--- lab_1/start.py
+++ lab_1/start.py
@@ -3,7 +3,6 @@ Language detection starter
 """
 
 import os
-import main
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_TEXTS_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'texts')
@@ -24,31 +23,8 @@ if __name__ == '__main__':
             file_to_read:
         unknown_text = file_to_read.read()
 
-    EXPECTED = 'en'
+    EXPECTED = ''
     RESULT = ''
-    TOP_N = 7
-    unknown_profile = main.create_language_profile('unknown_text', unknown_text, [])
-
-    # compare language detective results
-    profile_en_10 = main.load_profile(os.path.join(PATH_TO_PROFILES_FOLDER, 'en.json'))
-    profile_de_10 = main.load_profile(os.path.join(PATH_TO_PROFILES_FOLDER, 'de.json'))
-    profile_la_10 = main.load_profile(os.path.join(PATH_TO_PROFILES_FOLDER, 'la.json'))
-    profiles_10 = [profile_en_10, profile_de_10, profile_la_10]
-    # we may save profiles in json file
-    main.save_profile(unknown_profile)
-
-    RESULT = main.detect_language_advanced(unknown_profile, profiles_10, [], TOP_N)
-
-    # profile_en_8 = main.create_language_profile("en", en_text, [])
-    # profile_de_8 = main.create_language_profile("de", de_text, [])
-    # profile_la_8 = main.create_language_profile("la", la_text, [])
-    # profiles_8 = [profile_en_8, profile_de_8, profile_la_8]
-    # RESULT_8 = main.detect_language_advanced(unknown_profile, profiles_8, [], TOP_N)
-
-    # if RESULT_10 == RESULT_8:
-    # print("Great!", RESULT_10, "==", RESULT_8)
-    # else:
-    # print("Something is wrong!")
 
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     # assert RESULT, 'Detection not working'
