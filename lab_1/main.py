98eeed03ab1d3c9ca65314b9ae94655b02edae3e
danalifshits <danessia03@gmail.com>
2021-10-06 11:50:41 +0300

    удалила pass из нереализованных функций

--- lab_1/main.py
+++ lab_1/main.py
@@ -121,8 +121,8 @@ def get_top_n_words(freq_dict, top_n):
 
     return top_list
 
-'''
-def create_language_profile(language: str, text: str, stop_words: list) -> dict or None:
+
+def create_language_profile(language: str, text: str, stop_words: list):
     """
     Creates a language profile
     :param language: a language
@@ -130,10 +130,9 @@ def create_language_profile(language: str, text: str, stop_words: list) -> dict 
     :param stop_words: a list of stop words
     :return: a dictionary with three keys – name, freq, n_words
     """
-    pass
 
 
-def compare_profiles(unknown_profile: dict, profile_to_compare: dict, top_n: int) -> float or None:
+def compare_profiles(unknown_profile: dict, profile_to_compare: dict, top_n: int):
     """
     Compares profiles and calculates the distance using top n words
     :param unknown_profile: a dictionary
@@ -153,10 +152,9 @@ def detect_language(unknown_profile: dict, profile_1: dict, profile_2: dict, top
     :param top_n: a number of the most common words
     :return: a language
     """
-    pass
 
 
-def compare_profiles_advanced(unknown_profile: dict, profile_to_compare: dict, top_n: int) -> list or None:
+def compare_profiles_advanced(unknown_profile: dict, profile_to_compare: dict, top_n: int):
     """
     Compares profiles and calculates some advanced parameters
     :param unknown_profile: a dictionary
@@ -165,10 +163,9 @@ def compare_profiles_advanced(unknown_profile: dict, profile_to_compare: dict, t
     :return: a dictionary with 7 keys – name, score, common, sorted_common, max_length_word,
     min_length_word, average_token_length
     """
-    pass
 
 
-def detect_language_advanced(unknown_profile: dict, profiles: list, languages: list, top_n: int) -> str or None:
+def detect_language_advanced(unknown_profile: dict, profiles: list, languages: list, top_n: int):
     """
     Detects the language of an unknown profile within the list of possible languages
     :param unknown_profile: a dictionary
@@ -177,23 +174,19 @@ def detect_language_advanced(unknown_profile: dict, profiles: list, languages: l
     :param top_n: a number of the most common words
     :return: a language
     """
-    pass
 
 
-def load_profile(path_to_file: str) -> dict or None:
+def load_profile(path_to_file: str):
     """
     Loads a language profile
     :param path_to_file: a path
     :return: a dictionary with three keys – name, freq, n_words
     """
-    pass
 
 
-def save_profile(profile: dict) -> int:
+def save_profile(profile: dict):
     """
     Saves a language profile
     :param profile: a dictionary
     :return: 0 if everything is ok, 1 if not
     """
-    pass
-'''
