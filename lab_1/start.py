a327d66acb6d26a81aebfcb11cc3f667d208d073
Burakovan <burakova.02@mail.ru>
2021-10-07 22:08:09 +0300

    score changed

--- lab_1/start.py
+++ lab_1/start.py
@@ -3,12 +3,12 @@ Language detection starter
 """
 
 import os
+from main import create_language_profile, detect_language
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_TEXTS_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'texts')
 
 if __name__ == '__main__':
-
     with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'en.txt'), 'r', encoding='utf-8') as file_to_read:
         en_text = file_to_read.read()
 
@@ -19,7 +19,12 @@ if __name__ == '__main__':
             file_to_read:
         unknown_text = file_to_read.read()
 
+    en_profile = create_language_profile('en', en_text, [])
+    de_profile = create_language_profile('de', de_text, [])
+    unknown_profile = create_language_profile('unknown', unknown_text, [])
+
     EXPECTED = 'en'
-    RESULT = ''
+    RESULT = detect_language(unknown_profile, en_profile, de_profile, 4)
+    print(RESULT)
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
