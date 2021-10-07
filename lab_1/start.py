372cd800a626e8fea717d93ec47fda0f9588ae1a
Gorbacheva Sasha <90442800+GorbachevaSasha@users.noreply.github.com>
2021-10-07 19:25:34 +0300

    Update start.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -9,7 +9,7 @@ PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_TEXTS_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'texts')
 PATH_TO_PROFILE_FOLDER = os.path.join(PATH_TO_LAB_FOLDER,'profiles')
 
-if __name__ == '__main__':
+if _name_ == '__main__':
 
     with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'en.txt'), 'r', encoding='utf-8') as file_to_read:
         en_text = file_to_read.read()
@@ -28,6 +28,20 @@ if __name__ == '__main__':
     de_profile = main.create_language_profile('de', de_text, [])
     la_profile = main.create_language_profile('la', la_text, [])
     unknown_profile = main.create_language_profile('unk', unknown_text, [])
+
+    EXPECTED = 'en'
+
+    de_and_la_result = main.detect_language_advanced(unknown_profile,[de_profile,la_profile],[],4)
+    print(de_and_la_result)
+
+    all_profiles = [de_profile,en_profile,la_profile]
+    new_all_profiles = [main.load_profile(os.path.join(PATH_TO_PROFILE_FOLDER,'de.json')),
+                main.load_profile(os.path.join(PATH_TO_PROFILE_FOLDER,'en.json')),
+                main.load_profile(os.path.join(PATH_TO_PROFILE_FOLDER,'la.json'))]
+    all_profiles_result = main.detect_language_advanced(unknown_profile,all_profiles,[],4)
+    new_all_profiles_result = main.detect_language_advanced(unknown_profile,new_all_profiles,[],4)
+    print(all_profiles_result)
+    print(new_all_profiles_result)
 
     RESULT = ''
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
