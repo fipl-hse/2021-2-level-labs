a68f76b824b8ea63b83e5c8cd013799bfa9a7ee4
titovakaterina <90442736+titovakaterina@users.noreply.github.com>
2021-10-08 09:47:10 +0300

    Update start.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -3,10 +3,10 @@ Language detection starter
 """
 
 import os
+import main
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_TEXTS_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'texts')
-PATH_TO_PROFILES_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'profiles')
 
 if __name__ == '__main__':
 
@@ -16,16 +16,20 @@ if __name__ == '__main__':
     with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'de.txt'), 'r', encoding='utf-8') as file_to_read:
         de_text = file_to_read.read()
 
-    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'la.txt'), 'r', encoding='utf-8') as file_to_read:
-        la_text = file_to_read.read()
-
-    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'unknown.txt'), 'r', encoding='utf-8') as \
-            file_to_read:
+    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'unknown.txt'), 'r', encoding='utf-8') as file_to_read:
         unknown_text = file_to_read.read()
 
-    EXPECTED = 'en'
-    RESULT = ''
+    unknown = main.create_language_profile("unknown", unknown_text, [])
+    english = main.create_language_profile("english", en_text, [])
+    deutsch = main.create_language_profile("german", de_text, [])
+
+    all_languages = [english, deutsch]
+
+    TOP_N = 5
+    EXPECTED = 'english'
+    RESULT = main.detect_language(unknown, english, deutsch, TOP_N)
+
+    print('The result is', RESULT)
 
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    # assert RESULT, 'Detection not working'
-    assert EXPECTED == RESULT, 'Detection not working'
+    assert RESULT == EXPECTED, 'Detection not working'
