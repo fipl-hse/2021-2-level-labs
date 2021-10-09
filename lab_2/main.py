7cfa429cda34c0073ac2576311b4dff3eaed6e21
ArturIvanov228 <90447341+ArturIvanov228@users.noreply.github.com>
2021-10-09 15:39:16 +0300

    Update main.py

--- lab_2/main.py
+++ lab_2/main.py
@@ -32,7 +32,15 @@ def get_language_profiles(texts_corpus: list, language_labels: list) -> dict or 
     :param language_labels: a list of given language labels
     :return: a dictionary of dictionaries - language profiles
     """
-    pass
+        if not isinstance(texts_corpus, list) or not isinstance(language_labels, list):
+        return None
+    result = {}
+    for i in range(len(texts_corpus)):
+        if not isinstance(texts_corpus[i], list) or not isinstance(language_labels[i], str):
+            return None
+        else:
+            result[language_labels[i]] = get_freq_dict(texts_corpus[i])
+    return result
 
 
 def get_language_features(language_profiles: dict) -> list or None:
