09dc596fc650521b141aa5ba08a78f84cd91f7b9
lenazagg <lenazagovorova@gmail.com>
2021-10-11 12:55:34 +0300

    Changes

--- lab_2/main.py
+++ lab_2/main.py
@@ -23,6 +23,7 @@ def get_freq_dict(tokens: list) -> dict or None:
             freq_dict[token] += 1 / len(tokens)
         else:
             freq_dict[token] = 1 / len(tokens)
+
     return freq_dict
 
 
