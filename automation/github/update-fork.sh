3d763151ba448d38bac3a96e7ad45508cf2aeaac
Andrey Kashchikhin <akashchi@akashchi-mac01.local>
2021-10-14 22:27:03 +0300

    rm unused

--- automation/github/update-fork.sh
+++ automation/github/update-fork.sh
@@ -38,8 +38,6 @@ FORKED_URL="https://x-access-token:${AUTH_TOKEN}@${FORKED_URL}"
 git remote add upstream "${FORKED_URL}"
 git fetch upstream
 
-git remote -v
-
 git checkout upstream/main
 
 if [[ $FORCE -eq 1 ]]; then
