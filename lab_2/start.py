7176a5fde15b59f7861a5f67ab9b67f2f325e0c6
eskondrashova <90447381+eskondrashova@users.noreply.github.com>
2021-10-17 23:16:30 +0300

    Update start.py

--- lab_2/start.py
+++ lab_2/start.py
@@ -3,19 +3,9 @@ Language detection starter
 """
 
 import os
-from lab_2.main import (
-    tokenize,
-    remove_stop_words,
-    get_freq_dict,
-    get_language_profiles,
-    get_language_features,
-    get_text_vector,
-    calculate_distance,
-    predict_language_score,
-    predict_language_knn,
-    calculate_distance_sparse,
-    predict_language_knn_sparse
-)
+from lab_2.main import (tokenize, remove_stop_words,
+                        get_language_profiles, get_sparse_vector,
+                        predict_language_knn_sparse)
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_PROFILES_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'profiles')
@@ -51,6 +41,30 @@ if __name__ == '__main__':
         UNKNOWN_SAMPLES = file_to_read.read().split('[TEXT]')[1:]
 
     EXPECTED = ['de', 'eng', 'lat']
-    RESULT = ''
+    STOP_WORDS = []
+    texts_corpus = []
+    lanuage_labels = []
+    for text in DE_SAMPLES:
+        texts_corpus.append(remove_stop_words(tokenize(text), STOP_WORDS))
+        lanuage_labels.append('de')
+    for text in EN_SAMPLES:
+        texts_corpus.append(remove_stop_words(tokenize(text), STOP_WORDS))
+        lanuage_labels.append('eng')
+    for text in LAT_SAMPLES:
+        texts_corpus.append(remove_stop_words(tokenize(text), STOP_WORDS))
+        lanuage_labels.append('lat')
+    language_profiles = get_language_profiles(texts_corpus, language_labels)
+    known_text_vectors = []
+    for text in texts_corpus:
+        known_text_vectors.append(get_sparse_vector(text, language_profiles))
+    KNN = 3
+    RESULT = []
+    for text in UNKNOWN_SAMPLES:
+        unknown_text = remove_stop_words(tokenize(unk_text), STOP_WORDS)
+        unknown_text_vector = get_sparse_vector(unknown_text, language_profiles)
+        predicted_lang = predict_language_knn_sparse(unknown_text_vector, known_text_vectors,
+                                                     language_labels, KNN)
+        RESULT.append(predicted_lang[0])
+    
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
