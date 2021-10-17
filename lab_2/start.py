37ed35507091fd504515ed4aa663799927c9ffbc
lyutl <rajda02@gmail.com>
2021-10-18 02:47:09 +0300

    Add start.py

--- lab_2/start.py
+++ lab_2/start.py
@@ -3,6 +3,11 @@ Language detection starter
 """
 
 import os
+from lab_2.main import (tokenize,
+                        remove_stop_words,
+                        get_language_profiles,
+                        get_sparse_vector,
+                        predict_language_knn_sparse)
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_PROFILES_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'profiles')
@@ -38,6 +43,31 @@ if __name__ == '__main__':
         UNKNOWN_SAMPLES = file_to_read.read().split('[TEXT]')[1:]
 
     EXPECTED = ['de', 'eng', 'lat']
-    RESULT = ''
+    RESULT = []
+
+    text_corpus = []
+    lang_labels = []
+    STOP_WORDS = []
+    KNN = 3
+
+    for text in DE_SAMPLES:
+        text_corpus.append(remove_stop_words(tokenize(text), STOP_WORDS))
+        lang_labels.append('de')
+    for text in EN_SAMPLES:
+        text_corpus.append(remove_stop_words(tokenize(text), STOP_WORDS))
+        lang_labels.append('eng')
+    for text in LAT_SAMPLES:
+        text_corpus.append(remove_stop_words(tokenize(text), STOP_WORDS))
+        lang_labels.append('lat')
+
+    lang_profiles = get_language_profiles(text_corpus, lang_labels)
+    known_vectors = [get_sparse_vector(text, lang_profiles) for text in text_corpus]
+
+    for text in UNKNOWN_SAMPLES:
+        unknown_text = remove_stop_words(tokenize(text), STOP_WORDS)
+        unknown_vector = get_sparse_vector(unknown_text, lang_profiles)
+        prediction = predict_language_knn_sparse(unknown_vector, known_vectors, lang_labels, KNN)
+        RESULT.append(prediction[0])
+
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
