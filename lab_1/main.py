9ad88167d204ed092ff0d3c999b85a4ea4ccbdfe
WhiteJaeger <logerk3@gmail.com>
2021-10-17 19:36:23 +0400

    use lab 1 from the original repo

--- lab_1/main.py
+++ lab_1/main.py
@@ -13,22 +13,12 @@ def tokenize(text: str) -> list or None:
     :param text: a text
     :return: a list of lower-cased tokens without punctuation
     """
-    if not isinstance(text, str):   # проверяет, принадлежит ли элемент тому или иному классу(возвращает True/False)
+    if not isinstance(text, str):
         return None
     invaluable_trash = ['`', '~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '-', '+',
                         '=', '{', '[', ']', '}', '|', '\\', ':', ';', '"', "'", '<', ',', '>',
                         '.', '?', '/', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0']
     text = text.lower()
-<<<<<<< HEAD
-    text = re.sub('[^a-züöäß \n]', '', text)    # ищем небуквенные символы
-    # ищет подстроку по шаблону и заменяет ее на пробел; пропускает перенос строки
-    text = text.split()
-    return text
-
-
-def remove_stop_words(tokens: list,
-                      stop_words: list) -> list or None:
-=======
     for symbols in invaluable_trash:
         text = text.replace(symbols, '')
     tokens = text.split()
@@ -36,7 +26,6 @@ def remove_stop_words(tokens: list,
 
 
 def remove_stop_words(tokens: list, stop_words: list) -> list or None:
->>>>>>> 94359393a8b8a88ab306da1febf438dd20af6781
     """
     Removes stop words
     :param tokens: a list of tokens
@@ -45,17 +34,12 @@ def remove_stop_words(tokens: list, stop_words: list) -> list or None:
     """
     if not isinstance(tokens, list) or not isinstance(stop_words, list):
         return None
-<<<<<<< HEAD
-    tokens = [n for n in tokens if n not in stop_words]
-    return tokens
-
-=======
     new_tokens = []
     for word in tokens:
         if word not in stop_words:
             new_tokens.append(word)
     return new_tokens
->>>>>>> 94359393a8b8a88ab306da1febf438dd20af6781
+
 
 def calculate_frequencies(tokens: list) -> dict or None:
     """
@@ -86,19 +70,9 @@ def get_top_n_words(freq_dict: dict, top_n: int) -> list or None:
     """
     if not isinstance(freq_dict, dict) or not isinstance(top_n, int):
         return None
-<<<<<<< HEAD
-    sorted_most_common = dict(sorted(freq_dict.items(), key=lambda x: -x[1]))
-    # key - уточняет по какому критерию идет сортировка,
-    # х - элемент, х[1] - частота этого элемента(токена); для того чтобы сортировка шла по убыванию ставим "-"
-    # Лямбда принимает любое количество аргументов (или ни одного) и состоит из одного выражения.
-    common_words = list(sorted_most_common)
-    common_words = common_words[:top_n]
-    return common_words
-=======
     # sort by keys and take the top_n tokens from the list of sorted tokens
     top_n_words = sorted(freq_dict, key=freq_dict.get, reverse=True)[:top_n]
     return top_n_words
->>>>>>> 94359393a8b8a88ab306da1febf438dd20af6781
 
 
 def create_language_profile(language: str, text: str, stop_words: list) -> dict or None:
@@ -135,18 +109,6 @@ def compare_profiles(unknown_profile: dict, profile_to_compare: dict, top_n: int
             or not isinstance(profile_to_compare, dict)
             or not isinstance(top_n, int)):
         return None
-<<<<<<< HEAD
-    profile_compared = profile_to_compare['freq']
-    profile_compared_top = get_top_n_words(profile_compared, top_n)
-    unknown_profile_needed = unknown_profile['freq']
-    unknown_profile_top = get_top_n_words(unknown_profile_needed, top_n)
-    common_tokens = 0
-    for i in unknown_profile_top:
-        if i in profile_compared_top:
-            common_tokens += 1
-    distance = round(common_tokens/(len(unknown_profile_top)), 2)  # доля пересекающихся частотных слов.
-    return distance
-=======
     # use function get_top_n_words
     top_n_words_unknown = get_top_n_words(unknown_profile["freq"], top_n)
     top_n_words_compare = get_top_n_words(profile_to_compare["freq"], top_n)
@@ -155,7 +117,6 @@ def compare_profiles(unknown_profile: dict, profile_to_compare: dict, top_n: int
     # find share of common tokens
     share_of_common_things = round(len(common_things)/len(top_n_words_unknown), 2)
     return share_of_common_things
->>>>>>> 94359393a8b8a88ab306da1febf438dd20af6781
 
 
 def detect_language(unknown_profile: dict,
@@ -202,31 +163,6 @@ def compare_profiles_advanced(unknown_profile: dict,
             or not isinstance(profile_to_compare, dict)
             or not isinstance(top_n, int)):
         return None
-<<<<<<< HEAD
-    profile_compared_top1 = get_top_n_words(profile_to_compare['freq'], top_n)
-    unknown_profile_top = get_top_n_words(unknown_profile['freq'], top_n)
-    most_common_words1 = []
-    for i in profile_compared_top1:
-        if i in unknown_profile_top:
-            most_common_words1.append(i)
-    common_score = len(most_common_words1)/len(unknown_profile_top)  # доля пересекающихся частотных слов.
-    words = list(profile_to_compare['freq'].keys())
-    max_length = max(words, key=len)
-    min_length = min(words, key=len)
-    length_word = 0
-    for i, word in enumerate(words):  # enumerate() позволяет перебирать элементов, отслеживая индекс текущего элемента.
-        if word in words:
-            length_word += len(words[i])
-    average_length = length_word / len(words)
-    comp_prof_adv = {'name': profile_to_compare['name'],
-                     'score': common_score,
-                     'common': most_common_words1,
-                     'sorted_common': sorted(most_common_words1),
-                     'max_length_word': max_length,
-                     'min_length_word': min_length,
-                     'average_token_length': average_length}
-    return comp_prof_adv
-=======
     # use function get_top_n_words to get common and sorted_common
     top_n_words_unknown = get_top_n_words(unknown_profile["freq"], top_n)
     top_n_words_compare = get_top_n_words(profile_to_compare["freq"], top_n)
@@ -254,7 +190,6 @@ def compare_profiles_advanced(unknown_profile: dict,
               'average_token_length': average_token_length,
               'sorted_common': sorted_common}
     return report
->>>>>>> 94359393a8b8a88ab306da1febf438dd20af6781
 
 
 def detect_language_advanced(unknown_profile: dict,
@@ -283,37 +218,6 @@ def detect_language_advanced(unknown_profile: dict,
     reports = sorted(reports, key=lambda x: x["score"], reverse=True)
     if not reports:
         return None
-<<<<<<< HEAD
-    max_score = [max(score)]
-    result_score = {}
-    keys = dict_score.keys()
-    for k in keys:
-        for i in max_score:
-            if dict_score[k] == i:
-                result_score[k] = i
-    result = list(sorted(result_score.keys()))
-    return result[0]
-
-
-# def load_profile(path_to_file: str) -> dict or None:
-#     """
-#     Loads a language profile
-#     :param path_to_file: a path
-#     :return: a dictionary with three keys – name, freq, n_words
-#     """
-#     pass
-#
-#
-# def save_profile(profile: dict) -> int:
-#     """
-#     Saves a language profile
-#     :param profile: a dictionary
-#     :return: 0 if everything is ok, 1 if not
-#     """
-#     pass
-
-#need
-=======
     # sort in alphabetically order if some languages have the same max scores
     # create the list with only scores and count the max score-element in it
     list_with_only_scores = []
@@ -360,4 +264,3 @@ def save_profile(profile: dict) -> int:
     with open(path_to_file, "w", encoding="utf-8") as file:
         json.dump(profile, file)
     return 0
->>>>>>> 94359393a8b8a88ab306da1febf438dd20af6781
