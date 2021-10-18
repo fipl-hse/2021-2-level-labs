f98811ba4bef158cec89af5c49a98e0cc370f505
Vasilisa-Blyudova <90454966+Vasilisa-Blyudova@users.noreply.github.com>
2021-10-18 23:26:57 +0300

    start.py

--- lab_2/start.py
+++ lab_2/start.py
@@ -3,6 +3,7 @@ Language detection starter
 """
 
 import os
+import main
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_PROFILES_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'profiles')
@@ -38,6 +39,30 @@ if __name__ == '__main__':
         UNKNOWN_SAMPLES = file_to_read.read().split('[TEXT]')[1:]
 
     EXPECTED = ['de', 'eng', 'lat']
-    RESULT = ''
+    RESULT = []
+    stop_words = []
+    texts_corpus = []
+    language_labels = []
+    known_text_vectors = []
+    k = 3
+    for de_text in DE_SAMPLES:
+        texts_corpus.append(main.remove_stop_words(main.tokenize(de_text), stop_words))
+        language_labels.append('de')
+    for en_text in EN_SAMPLES:
+        texts_corpus.append(main.remove_stop_words(main.tokenize(en_text), stop_words))
+        language_labels.append('eng')
+    for lat_text in LAT_SAMPLES:
+        texts_corpus.append(main.remove_stop_words(main.tokenize(lat_text), stop_words))
+        language_labels.append('lat')
+    language_profiles = main.get_language_profiles(texts_corpus, language_labels)
+    for text in texts_corpus:
+        known_text_vectors.append(main.get_sparse_vector(text, language_profiles))
+    for unknown_texts in UNKNOWN_SAMPLES:
+        unk_text = main.remove_stop_words(main.tokenize(unknown_texts), stop_words)
+        unknown_text_vector = main.get_sparse_vector(unk_text, language_profiles)
+        list_with_language_and_min_distance = main.predict_language_knn_sparse(unknown_text_vector,
+                                                                               known_text_vectors,
+                                                                               language_labels, k)
+        RESULT.append(list_with_language_and_min_distance[0])
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
