ed8fb4dc9422760716eff13423bca31a6b872f21
lenazagg <lenazagovorova@gmail.com>
2021-10-18 17:08:35 +0300

    changes

--- lab_2/main.py
+++ lab_2/main.py
@@ -78,7 +78,7 @@ def get_language_features(language_profiles: dict) -> list or None:
 
     for freq_dict in language_profiles.values():
         for key in freq_dict:
-            features.extend(key)
+            features.append(key)
             features = sorted(features)
     return features
 
