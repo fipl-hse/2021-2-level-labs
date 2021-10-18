d3b62b9b619d0a12271dc4164738d84e797a1f68
tanynova99 <balabolka2000@gmail.com>
2021-10-18 23:05:54 +0300

    Wrote start.py

--- lab_2/start.py
+++ lab_2/start.py
@@ -38,8 +38,43 @@ if __name__ == '__main__':
               'r', encoding='utf-8') as file_to_read:
         UNKNOWN_SAMPLES = file_to_read.read().split('[TEXT]')[1:]
 
-
+    RESULT = []
     EXPECTED = ['de', 'eng', 'lat']
-    RESULT = ''
+
+    corpus = []
+    labels = []
+    stop_words = []
+    k = 3
+    # creating corpus of German, English and Latin
+    # usual tokenization of the text to get the corpus
+    # appending the language name in labels
+    for text in DE_SAMPLES:
+        corpus.append(remove_stop_words(tokenize(text), stop_words))
+        labels.append('de')
+    for text in EN_SAMPLES:
+        corpus.append(remove_stop_words(tokenize(text), stop_words))
+        labels.append('eng')
+    for text in LAT_SAMPLES:
+        corpus.append(remove_stop_words(tokenize(text), stop_words))
+        labels.append('lat')
+
+    # creating the labels but using numeration instead of names
+    # used for calculating the unique variable and vectors from all known texts
+    index_labels = [str(i) for i in range(len(corpus))]
+    # getting profiles with their indexes {0: "first_text_freqs", 1: "second_text_freqs" etc.}
+    # and unique words thorough all of them
+    index_label_profiles = get_language_profiles(corpus, index_labels)
+    unique = get_language_features(index_label_profiles)
+    # using these profiles to create a bunch of known vectors
+    vectors = [get_sparse_vector(text, index_label_profiles) for text in corpus]
+
+    # tokenizing the unknown text
+    # guessing the language of each sample
+    for text in UNKNOWN_SAMPLES:
+        unknown_text = remove_stop_words(tokenize(text), stop_words)
+        unknown_vector = get_sparse_vector(unknown_text, index_label_profiles)
+        guess = predict_language_knn_sparse(unknown_vector, vectors, labels, k)
+        RESULT.append(guess[0])
+
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT, 'Detection not working'
+    assert RESULT == EXPECTED, 'Detection not working'
