f1f2f2d4b9525b8d199b8f537808638e826f7d9d
esgolovleva <golovleva.katerina@mail.ru>
2021-10-19 23:57:27 +0300

    start.py

--- lab_2/start.py
+++ lab_2/start.py
@@ -3,11 +3,9 @@ Language detection starter
 """
 
 import os
-from main import tokenize
-from main import remove_stop_words
-from main import get_language_profiles
-from main import get_sparse_vector
-from main import predict_language_knn_sparse
+from main import tokenize, remove_stop_words
+from lab_2.main import get_language_profiles, get_sparse_vector, \
+    predict_language_knn_sparse
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_PROFILES_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'profiles')
