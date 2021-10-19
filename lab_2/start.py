0995acb2bf61efe80960708ac0564ae1a38c3487
KoykovaAleksandra <madam.koykova@mail.ru>
2021-10-19 16:00:12 +0300

    Add start.py

--- lab_2/start.py
+++ lab_2/start.py
@@ -3,6 +3,8 @@ Language detection starter
 """
 
 import os
+from lab_2.main import (tokenize, remove_stop_words, get_language_profiles, get_sparse_vector,
+                        predict_language_knn_sparse)
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_PROFILES_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'profiles')
@@ -37,7 +39,32 @@ if __name__ == '__main__':
               'r', encoding='utf-8') as file_to_read:
         UNKNOWN_SAMPLES = file_to_read.read().split('[TEXT]')[1:]
 
+    stop_words = []
+    texts_corpus = []
+    language_labels = []
+    for text in EN_SAMPLES:
+        texts_corpus.append(remove_stop_words(tokenize(text), stop_words))
+        language_labels.append('eng')
+    for text in DE_SAMPLES:
+        texts_corpus.append(remove_stop_words(tokenize(text), stop_words))
+        language_labels.append('de')
+    for text in LAT_SAMPLES:
+        texts_corpus.append(remove_stop_words(tokenize(text), stop_words))
+        language_labels.append('lat')
+    language_profiles = get_language_profiles(texts_corpus, language_labels)
+    known_text_vectors = []
+    for text in texts_corpus:
+        known_text_vectors.append(get_sparse_vector(text, language_profiles))
+    k = 3
+    RESULT = []
+    for text in UNKNOWN_SAMPLES:
+        unknown_text = remove_stop_words(tokenize(text), stop_words)
+        unknown_text_vector = get_sparse_vector(unknown_text, language_profiles)
+        predict_language = predict_language_knn_sparse(unknown_text_vector, known_text_vectors,
+                                                       language_labels, k)
+        RESULT.append(predict_language[0])
+
+
     EXPECTED = ['de', 'eng', 'lat']
-    RESULT = ''
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
