835a105a9a046d04489ad364651ab980ef5c9e89
veronicamanukyan <vermanuk@mail.ru>
2021-10-19 16:51:24 +0300

    Add code

--- lab_2/start.py
+++ lab_2/start.py
@@ -3,6 +3,8 @@ Language detection starter
 """
 
 import os
+from lab_2.main import (tokenize, remove_stop_words, get_language_profiles,
+                        get_text_vector, predict_language_knn)
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_PROFILES_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'profiles')
@@ -38,6 +40,29 @@ if __name__ == '__main__':
         UNKNOWN_SAMPLES = file_to_read.read().split('[TEXT]')[1:]
 
     EXPECTED = ['de', 'eng', 'lat']
-    RESULT = ''
+    RESULT = []
+    stop_words = []
+    texts_corpus = []
+    language_labels = []
+    k = 3
+    for text in DE_SAMPLES:
+        texts_corpus.append(remove_stop_words(tokenize(text), stop_words))
+        language_labels.append('de')
+    for text in EN_SAMPLES:
+        texts_corpus.append(remove_stop_words(tokenize(text), stop_words))
+        language_labels.append('eng')
+    for text in LAT_SAMPLES:
+        texts_corpus.append(remove_stop_words(tokenize(text), stop_words))
+        language_labels.append('lat')
+    language_profiles = get_language_profiles(texts_corpus, language_labels)
+    known_text_vectors = []
+    for text in texts_corpus:
+        known_text_vectors.append(get_text_vector(text, language_profiles))
+    for text in UNKNOWN_SAMPLES:
+        unknown_text = remove_stop_words(tokenize(text), stop_words)
+        unknown_text_vector = get_text_vector(unknown_text, language_profiles)
+        predicted_language = predict_language_knn(unknown_text_vector,
+                                                  known_text_vectors, language_labels, k)
+        RESULT.append(predicted_language[0])
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
