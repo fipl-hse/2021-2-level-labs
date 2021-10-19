1f75745d209c8cca93b4cb893dedc51abacc2edf
lenazagg <lenazagovorova@gmail.com>
2021-10-19 12:23:25 +0300

    changes

--- lab_2/main.py
+++ lab_2/main.py
@@ -99,8 +99,9 @@ def get_text_vector(original_text: list, language_profiles: dict) -> list or Non
     for unique_word in get_language_features(language_profiles):
         if unique_word in original_text:
             for freq_dict in language_profiles.values():
-                if unique_word in freq_dict:
-                    vector.append(freq_dict[unique_word])
+                for key in freq_dict:
+                    if key == unique_word:
+                        vector.append(freq_dict[key])
         else:
             vector.append(0)
 
