0de39f83d73d320a2c7c8beb962badaa1fe59570
Azzruge <veselovsergeya@gmail.com>
2021-10-19 23:33:36 +0300

    okazalsja on obichnim smertnim umer on na mostovoj

--- lab_2/start.py
+++ lab_2/start.py
@@ -3,6 +3,19 @@ Language detection starter
 """
 
 import os
+from main import get_freq_dict
+from main import get_language_features
+from main import get_text_vector
+from main import get_sparse_vector
+from main import get_language_profiles
+from main import calculate_distance_sparse
+from main import calculate_distance
+from main import calculate_distance_manhattan
+from main import predict_language_score
+from main import predict_language_knn
+from main import predict_language_knn_sparse
+from main import tokenize
+from main import remove_stop_words
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_PROFILES_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'profiles')
@@ -39,5 +52,38 @@ if __name__ == '__main__':
 
     EXPECTED = ['de', 'eng', 'lat']
     RESULT = ''
+    # создаем языковые профили
+    labels = ['eng', 'de', 'lat']
+    lat_tokens = remove_stop_words(tokenize(LAT_TEXT), [])
+    de_tokens = remove_stop_words(tokenize(DE_TEXT), [])
+    eng_tokens = remove_stop_words(tokenize(EN_TEXT), [])
+    corpus = [eng_tokens, de_tokens, lat_tokens]
+    language_profiles = get_language_profiles(corpus, labels)
+    # создаем список разреженных векторов известных текстов
+    known_tet_vectors = []
+    language_labels = []
+    for text in DE_SAMPLES:
+        tokens = remove_stop_words(tokenize(text), [])
+        known_tet_vectors.append(get_sparse_vector(tokens, language_profiles))
+        language_labels.append('de')
+    for text in EN_SAMPLES:
+        tokens = remove_stop_words(tokenize(text), [])
+        known_tet_vectors.append(get_sparse_vector(tokens, language_profiles))
+        language_labels.append('eng')
+    for text in LAT_SAMPLES:
+        tokens = remove_stop_words(tokenize(text), [])
+        known_tet_vectors.append(get_sparse_vector(tokens, language_profiles))
+        language_labels.append('lat')
+    # создаем разреженные векторы для неизвестных текстов
+    unknown_text_vectors = []
+    for text in UNKNOWN_SAMPLES:
+        tokens = remove_stop_words(tokenize(text), [])
+        unknown_text_vectors.append(get_sparse_vector(tokens, language_profiles))
+    # узнаем язык текстов
+    result = []
+    for vector in unknown_text_vectors:
+        result.append(predict_language_knn_sparse(vector, known_tet_vectors, language_labels, 3)[0])
+    print(result)
+    print(EXPECTED)
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT, 'Detection not working'
+    assert result, EXPECTED
