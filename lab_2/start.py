14773dbb516c86baa071223b671d9f10e98e48eb
KseniaEremeeva <irtn2002@yandex.ru>
2021-10-19 13:45:22 +0500

    Добавлен вызов функции

--- lab_2/start.py
+++ lab_2/start.py
@@ -3,6 +3,7 @@ Language detection starter
 """
 
 import os
+import main
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_PROFILES_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'profiles')
@@ -38,6 +39,36 @@ if __name__ == '__main__':
         UNKNOWN_SAMPLES = file_to_read.read().split('[TEXT]')[1:]
 
     EXPECTED = ['de', 'eng', 'lat']
-    RESULT = ''
+    RESULT = []
+
+    stop_words = []
+    texts_corpus = []
+    language_labels = []
+
+    language_profiles = main.get_language_profiles(texts_corpus, language_labels)
+    known_text_vectors = []
+    for text in texts_corpus:
+        known_text_vectors.append(main.get_text_vector(text, language_profiles))
+
+    for text in EN_SAMPLES:
+        texts_corpus.append(main.remove_stop_words(main.tokenize(text), stop_words))
+        language_labels.append('eng')
+    for text in DE_SAMPLES:
+        texts_corpus.append(main.remove_stop_words(main.tokenize(text), stop_words))
+        language_labels.append('de')
+    for text in LAT_SAMPLES:
+        texts_corpus.append(main.remove_stop_words(main.tokenize(text), stop_words))
+        language_labels.append('lat')
+
+    for text in UNKNOWN_SAMPLES:
+        unknown_text = main.remove_stop_words(main.tokenize(text), stop_words)
+        unknown_text_vector = main.get_text_vector(unknown_text, language_profiles)
+        predicted_language = main.predict_language_score(unknown_text_vector, known_text_vectors, language_labels)
+
+    RESULT.append(predicted_language)
+    print(RESULT)
+
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
+
+
