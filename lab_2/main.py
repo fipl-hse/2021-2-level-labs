31b6e63dc25395718141dcdc72949f762d8e64f4
ArturIvanov228 <90447341+ArturIvanov228@users.noreply.github.com>
2021-10-20 18:31:54 +0300

    Update main.py

--- lab_2/main.py
+++ lab_2/main.py
@@ -7,15 +7,8 @@ from lab_1.main import tokenize, remove_stop_words
 
 
 # 4
+
 def get_freq_dict(tokens: list) -> dict or None:
-    """
-    Calculates frequencies of given tokens
-    :param tokens: a list of tokens
-    :return: a dictionary with frequencies
-    """
-
-
-def get_freq_dict(tokens: list):
     if not isinstance(tokens, list):
         return None
     result = {}
