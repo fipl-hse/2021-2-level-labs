622689b114014d661ca94e1dc6ed93648e5875ec
froschalad <vaaladyshkin@edu.hse.ru>
2021-10-21 21:18:00 +0300

    start

--- lab_2/start.py
+++ lab_2/start.py
@@ -3,6 +3,7 @@ Language detection starter
 """
 
 import os
+from lab_2.main import tokenize, remove_stop_words, get_language_profiles, get_text_vector, predict_language_score
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_PROFILES_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'profiles')
@@ -36,6 +37,35 @@ if __name__ == '__main__':
     with open(os.path.join(PATH_TO_DATASET_FOLDER, 'unknown_samples.txt'),
               'r', encoding='utf-8') as file_to_read:
         UNKNOWN_SAMPLES = file_to_read.read().split('[TEXT]')[1:]
+
+    stop_words = []
+    texts_corpus = []
+    known_text_vectors = []
+    language_labels = []
+    RESULT = []
+    predict_language = []
+
+    language_profiles = get_language_profiles(texts_corpus, language_labels)
+
+    for text in texts_corpus:
+        known_text_vectors.append(get_text_vector(text, language_profiles))
+
+    for text in EN_SAMPLES:
+        texts_corpus.append(remove_stop_words(tokenize(text), stop_words))
+        language_labels.append('eng')
+    for text in DE_SAMPLES:
+        texts_corpus.append(remove_stop_words(tokenize(text), stop_words))
+        language_labels.append('de')
+    for text in LAT_SAMPLES:
+        texts_corpus.append(remove_stop_words(tokenize(text), stop_words))
+        language_labels.append('lat')
+
+    for text in UNKNOWN_SAMPLES:
+        unknown_text = remove_stop_words(tokenize(text), stop_words)
+        unknown_text_vector = get_text_vector(unknown_text, language_profiles)
+        predict_language = predict_language_score(unknown_text_vector, known_text_vectors, language_labels)
+
+    RESULT.append(predict_language)
 
     EXPECTED = ['de', 'eng', 'lat']
     RESULT = ''
