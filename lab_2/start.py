e141b0f0aaa86e0ff79fc9cf8db230a3b237f69e
lenazagg <lenazagovorova@gmail.com>
2021-10-21 15:12:00 +0300

    changes start.py but there's smth wrong

--- lab_2/start.py
+++ lab_2/start.py
@@ -3,6 +3,7 @@ Language detection starter
 """
 
 import os
+from lab_2.main import tokenize, remove_stop_words, get_language_profiles, get_text_vector, predict_language_score
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_PROFILES_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'profiles')
@@ -37,7 +38,35 @@ if __name__ == '__main__':
               'r', encoding='utf-8') as file_to_read:
         UNKNOWN_SAMPLES = file_to_read.read().split('[TEXT]')[1:]
 
+    stop_words = []
+    texts_corpus = []
+    known_text_vectors = []
+    language_labels = []
+    RESULT = []
+    predict_language = []
+
+    language_profiles = get_language_profiles(texts_corpus, language_labels)
+
+    for text in texts_corpus:
+        known_text_vectors.append(get_text_vector(text, language_profiles))
+
+    for text in DE_SAMPLES:
+        texts_corpus.append(remove_stop_words(tokenize(text), stop_words))
+        language_labels.append('de')
+    for text in EN_SAMPLES:
+        texts_corpus.append(remove_stop_words(tokenize(text), stop_words))
+        language_labels.append('eng')
+    for text in LAT_SAMPLES:
+        texts_corpus.append(remove_stop_words(tokenize(text), stop_words))
+        language_labels.append('lat')
+
+    for text in UNKNOWN_SAMPLES:
+        unknown_text = remove_stop_words(tokenize(text), stop_words)
+        unknown_text_vector = get_text_vector(unknown_text, language_profiles)
+        predict_language = predict_language_score(unknown_text_vector, known_text_vectors, language_labels)
+
+    RESULT.append(predict_language)
+
     EXPECTED = ['de', 'eng', 'lat']
-    RESULT = ''
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
