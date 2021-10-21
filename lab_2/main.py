6615dd2f37531e3514b75d6eb02df990f0d25bb7
KatyaKhl <90447360+KatyaKhl@users.noreply.github.com>
2021-10-21 22:29:57 +0300

    added 6 steps

--- lab_2/main.py
+++ lab_2/main.py
@@ -4,16 +4,31 @@ Language classification
 """
 
 from lab_1.main import tokenize, remove_stop_words
+import math
 
-
-# 4
+#4
 def get_freq_dict(tokens: list) -> dict or None:
     """
     Calculates frequencies of given tokens
     :param tokens: a list of tokens
     :return: a dictionary with frequencies
     """
-    pass
+    if not isinstance(tokens, list):
+        return None
+    freq_dict = {}
+    for token in tokens:
+        if not isinstance(token,str):
+            return None
+        if token in freq_dict:
+            freq_dict[token] += 1
+        else:
+            freq_dict[token] = 1
+    for token in freq_dict:
+        freq_dict[token] = round(freq_dict[token]/len(tokens), 5)
+    return freq_dict
+
+
+
 
 
 def get_language_profiles(texts_corpus: list, language_labels: list) -> dict or None:
@@ -24,7 +39,21 @@ def get_language_profiles(texts_corpus: list, language_labels: list) -> dict or 
     :param language_labels: a list of given language labels
     :return: a dictionary of dictionaries - language profiles
     """
-    pass
+    if not isinstance (texts_corpus, list) or not isinstance(language_labels, list):
+        return None
+    language_profiles = {}
+
+    for index in texts_corpus:
+        if not isinstance(index, list):
+            return None
+    for index in language_labels:
+        if not isinstance(index, str):
+            return None
+    for index in range(len(language_labels)):
+        language_profiles[language_labels[index]] = get_freq_dict(texts_corpus[index])
+    return language_profiles
+    
+
 
 
 def get_language_features(language_profiles: dict) -> list or None:
@@ -33,7 +62,16 @@ def get_language_features(language_profiles: dict) -> list or None:
         and sorts them in alphabetical order
     :param language_profiles: a dictionary of dictionaries - language profiles
     """
-    pass
+    if not isinstance(language_profiles, list):
+        return None
+    unique_words = []
+    for freq_dict in language_profiles.values():
+        unique_words.extend(list(freq_dict.keys()))
+    unique_words = sorted(unique_words)
+    return  unique_words
+
+
+
 
 
 def get_text_vector(original_text: list, language_profiles: dict) -> list or None:
@@ -43,7 +81,21 @@ def get_text_vector(original_text: list, language_profiles: dict) -> list or Non
     :param original_text: any tokenized text
     :param language_profiles: a dictionary of dictionaries - language profiles
     """
-    pass
+    if not (isinstance(original_text, list) or isinstance(language_profiles, list)):
+        return None
+    text_vector = []
+    for unique_words in get_language_features(language_profiles):
+        if unique_words in original_text:
+            for freq_dict in language_profiles.values():
+                if unique_words in freq_dict:
+                    text_vector.append(freq_dict[unique_words])
+        else:
+            text_vector.append(0)
+    return text_vector
+
+
+
+
 
 
 # 6
@@ -53,7 +105,21 @@ def calculate_distance(unknown_text_vector: list, known_text_vector: list) -> fl
     :param unknown_text_vector: vector for unknown text
     :param known_text_vector: vector for known text
     """
-    pass
+    if not (isinstance(unknown_text_vector, list) or isinstance(known_text_vector, list)):
+        return None
+    for element in unknown_text_vector:
+        if not isinstance(element, (float, int)):
+            return None
+    for element in known_text_vector:
+        if not isinstance(element, (float, int)):
+            return None
+    distance = 0
+    for index, number in enumerate(unknown_text_vector):
+        distance += (number - known_text_vector[index])**2
+        distance = round(math.sqrt(distance), 5)
+    return distance
+
+
 
 
 def predict_language_score(unknown_text_vector: list, known_text_vectors: list,
@@ -64,8 +130,25 @@ def predict_language_score(unknown_text_vector: list, known_text_vectors: list,
     :param known_text_vectors: a list of vectors for known texts
     :param language_labels: language labels for each known text
     """
-    pass
+    if not (isinstance(unknown_text_vector, list) or isinstance(known_text_vectors, list) or isinstance(language_labels, list)):
+        return None
+    for element in unknown_text_vector:
+        if not isinstance(element, (float,int)):
+            return None
+    for vector in known_text_vectors:
+        if not isinstance(vector, list):
+            return None
+    for label in language_labels:
+        if not isinstance(label,str):
+            return None
+    if len(known_text_vectors) != len(language_labels):
+        return None
 
+    distance = []
+    for vector in known_text_vectors:
+        distance.append(calculate_distance(unknown_text_vector, vector))
+    result_language_score = [language_labels[distance.index(min(distance))], min(distance)]
+    return result_language_score
 
 # 8
 def calculate_distance_manhattan(unknown_text_vector: list,
--- lab_2/start.py
+++ lab_2/start.py
@@ -3,6 +3,7 @@ Language detection starter
 """
 
 import os
+import main
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_PROFILES_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'profiles')
@@ -39,5 +40,29 @@ if __name__ == '__main__':
 
     EXPECTED = ['de', 'eng', 'lat']
     RESULT = ''
+    texts_corpus = []
+    language_labels = []
+    stop_words = []
+
+    language_profiles = main.get_language_profiles(texts_corpus, language_labels)
+    known_text_vectors = []
+    for text in texts_corpus:
+        known_text_vectors.append(main.get_text_vector(texts_corpus, language_labels))
+
+    for text in EN_SAMPLES:
+        texts_corpus.append(main.remove_stop_words(main.tokenize(text), stop_words))
+        language_labels.append('eng')
+    for text in DE_SAMPLES:
+        texts_corpus.append(main.remove_stop_words(main.tokenize(text), stop_words))
+        language_labels.append('de')
+    for text in LAT_SAMPLES:
+        texts_corpus.append(main.remove_stop_words(main.tokenize(text), stop_words))
+        language_labels.append('lat')
+    for text in UNKNOWN_SAMPLES:
+        unknown_text = main.remove_stop_words(main.tokenize(text), stop_words)
+        unknown_text_vector = main.get_text_vector(unknown_text, language_profiles)
+        predicted_language = main.predict_language_score(unknown_text_vector, known_text_vectors, language_labels)
+    RESULT.append(predicted_language)
+    print(RESULT)
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
