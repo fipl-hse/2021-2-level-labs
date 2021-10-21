1a893ba6f2e0fa9671ba1b109045a58609c9451c
Artem Samoylovsky <90442858+samoylovsky@users.noreply.github.com>
2021-10-21 15:52:21 +0300

    Update main.py

--- lab_2/main.py
+++ lab_2/main.py
@@ -195,7 +195,7 @@ def predict_language_knn(unknown_text_vector: list, known_text_vectors: list,
             return None
     dictionary_of_labels = {}
     for label in labels:
-        if label in dictionary_of labels:
+        if label in dictionary_of_labels:
             dictionary_of_labels[label] += 1
         else:
             dictionary_of_labels[label] = 1
