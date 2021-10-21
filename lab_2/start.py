24d34a1ca9523832182f30e746de30a2a6903949
KseniaEremeeva <irtn2002@yandex.ru>
2021-10-21 22:52:55 +0500

    Корректировки в коде

--- lab_2/start.py
+++ lab_2/start.py
@@ -39,33 +39,34 @@ if __name__ == '__main__':
         UNKNOWN_SAMPLES = file_to_read.read().split('[TEXT]')[1:]
 
     EXPECTED = ['de', 'eng', 'lat']
-    RESULT = []
 
     stop_words = []
     texts_corpus = []
     language_labels = []
+
+    for text in DE_SAMPLES:
+        texts_corpus.append(main.remove_stop_words(main.tokenize(text), stop_words))
+        language_labels.append('de')
+    for text in EN_SAMPLES:
+        texts_corpus.append(main.remove_stop_words(main.tokenize(text), stop_words))
+        language_labels.append('eng')
+    for text in LAT_SAMPLES:
+        texts_corpus.append(main.remove_stop_words(main.tokenize(text), stop_words))
+        language_labels.append('lat')
 
     language_profiles = main.get_language_profiles(texts_corpus, language_labels)
     known_text_vectors = []
     for text in texts_corpus:
         known_text_vectors.append(main.get_text_vector(text, language_profiles))
 
-    for text in EN_SAMPLES:
-        texts_corpus.append(main.remove_stop_words(main.tokenize(text), stop_words))
-        language_labels.append('eng')
-    for text in DE_SAMPLES:
-        texts_corpus.append(main.remove_stop_words(main.tokenize(text), stop_words))
-        language_labels.append('de')
-    for text in LAT_SAMPLES:
-        texts_corpus.append(main.remove_stop_words(main.tokenize(text), stop_words))
-        language_labels.append('lat')
+    RESULT = []
 
     for text in UNKNOWN_SAMPLES:
         unknown_text = main.remove_stop_words(main.tokenize(text), stop_words)
         unknown_text_vector = main.get_text_vector(unknown_text, language_profiles)
         predicted_language = main.predict_language_score(unknown_text_vector, known_text_vectors, language_labels)
+        RESULT.append(predicted_language[0])
 
-    RESULT.append(predicted_language)
     print(RESULT)
 
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
