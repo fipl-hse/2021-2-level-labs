b385ae808e1745fc29a3532e14e0e0d7072cd354
titovakaterina <titova.katerina2017@yandex.ru>
2021-10-22 00:32:59 +0300

    les_2

--- lab_2/main.py
+++ lab_2/main.py
@@ -217,7 +217,9 @@ def predict_language_knn(unknown_text_vector: list, known_text_vectors: list,
             dictionary_of_labels[label] += 1
         else:
             dictionary_of_labels[label] = 1
-
-    possible_label = max(dictionary_of_labels, key=dictionary_of_labels.get)
+    if dictionary_of_labels:
+        possible_label = max(dictionary_of_labels, key=dictionary_of_labels.get)
+    else:
+        return None
     possible_result = [possible_label, round(min(distances), 5)]
     return possible_result
