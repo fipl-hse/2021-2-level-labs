08d46d68c34fe4fd95f0ed1ba31a0658819717cb
AnnaSizykh <aasizykh@edu.hse.ru>
2021-10-23 22:22:22 +0300

    changes in start.py

--- lab_2/start.py
+++ lab_2/start.py
@@ -52,7 +52,8 @@ if __name__ == '__main__':
         tokens = remove_stop_words(tokenize(text), stop_words)
         tokens_corpus.append(tokens)
     language_profiles = get_language_profiles(tokens_corpus, ['eng', 'de', 'lat'])
-
+    #print(get_language_features(language_profiles))
+    #print(len(get_language_features(language_profiles)))
     #print(language_profiles)
     text_samples = {"de":DE_SAMPLES,"eng":EN_SAMPLES,"lat":LAT_SAMPLES}
     for lang, texts in text_samples.items():
@@ -60,7 +61,6 @@ if __name__ == '__main__':
             tok_text = remove_stop_words(tokenize(text),stop_words)
             known_text_vector = get_text_vector(tok_text, language_profiles)
             #print(len(known_text_vector))
-            known_texts_corpus.append(tok_text)
             known_text_vectors.append(known_text_vector)
             language_labels.append(lang)
 
@@ -68,8 +68,7 @@ if __name__ == '__main__':
     for text in UNKNOWN_SAMPLES:
         unknown_text = (remove_stop_words(tokenize(text),stop_words))
         unknown_text_vectors.append(get_text_vector(unknown_text, language_profiles))
-    ##print(predict_language_knn(unknown_text_vectors[0],
-    # known_text_vectors,language_labels, 1, 'manhattan'))
+    print(predict_language_knn(unknown_text_vectors[0],known_text_vectors,language_labels, 1, 'manhattan'))
 
 
     EXPECTED = ['de', 'eng', 'lat']
