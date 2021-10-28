beb7ca06fe372a9be2cd428984741a97055e1ed1
LapatanAA <aa_lapatan@mail.ru>
2021-10-28 17:44:04 +0300

    Lab2 for 8

--- lab_2/start.py
+++ lab_2/start.py
@@ -1,7 +1,9 @@
 """
 Language detection starter
 """
-
+from lab_1.main import tokenize, remove_stop_words
+from lab_2.main import get_freq_dict, get_language_profiles, get_text_vector, \
+    predict_language_knn
 import os
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
@@ -37,7 +39,34 @@ if __name__ == '__main__':
               'r', encoding='utf-8') as file_to_read:
         UNKNOWN_SAMPLES = file_to_read.read().split('[TEXT]')[1:]
 
+    en_tokens = tokenize(EN_TEXT)
+    de_tokens = tokenize(DE_TEXT)
+    lat_tokens = tokenize(LAT_TEXT)
+    corpus = [en_tokens, de_tokens, lat_tokens]
+    language_profiles = get_language_profiles(corpus, ['en', 'de', 'lat'])
+    unknown_txt_vector_first = get_text_vector(tokenize(UNKNOWN_SAMPLES[0]), language_profiles)
+    unknown_txt_vector_second = get_text_vector(tokenize(UNKNOWN_SAMPLES[1]), language_profiles)
+    unknown_txt_vector_third = get_text_vector(tokenize(UNKNOWN_SAMPLES[2]), language_profiles)
+    known_samples = []
+    known_txt_vectors = []
+    for element in EN_SAMPLES:
+        known_samples.append(element)
+    for element in DE_SAMPLES:
+        known_samples.append(element)
+    for element in LAT_SAMPLES:
+        known_samples.append(element)
+    for element in known_samples:
+        known_txt_vectors.append(get_text_vector(tokenize(element), language_profiles))
+    language_labels = ['en', 'en', 'en', 'en', 'en', 'de',
+                       'de', 'de', 'de', 'de', 'lat', 'lat', 'lat', 'lat', 'lat']
+    first_language = predict_language_knn(unknown_txt_vector_first,
+                                                 known_txt_vectors, language_labels, k=1)
+    second_language = predict_language_knn(unknown_txt_vector_second,
+                                                  known_txt_vectors, language_labels, k=1)
+    third_language = predict_language_knn(unknown_txt_vector_third,
+                                                 known_txt_vectors, language_labels, k=1)
     EXPECTED = ['de', 'eng', 'lat']
-    RESULT = ''
+    RESULT = [first_language[0], second_language[0], third_language[0]]
+    print(RESULT)
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
