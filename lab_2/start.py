19c0cee07dee57c30d639d70eebb33d77af5f406
Vlados13iq <znaminvlad@gmail.com>
2021-10-28 22:11:41 +0300

    fixed some errors in start.py

--- lab_2/start.py
+++ lab_2/start.py
@@ -4,7 +4,9 @@ Language detection starter
 
 import os
 from lab_1.main import tokenize, remove_stop_words
-from lab_2.main import  get_text_vector, get_language_profiles, predict_language_knn
+from lab_2.main import get_freq_dict, get_language_profiles, get_language_features, get_text_vector, \
+    calculate_distance, predict_language_score, calculate_distance_manhattan, predict_language_knn
+
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_PROFILES_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'profiles')
@@ -40,15 +42,24 @@ if __name__ == '__main__':
         UNKNOWN_SAMPLES = file_to_read.read().split('[TEXT]')[1:]
 
     EXPECTED = ['de', 'eng', 'lat']
-    RESULT = ''
-    labels = ['eng', 'de', 'lat']
+
     lat_tokens = remove_stop_words(tokenize(LAT_TEXT), [])
     de_tokens = remove_stop_words(tokenize(DE_TEXT), [])
     eng_tokens = remove_stop_words(tokenize(EN_TEXT), [])
+    unknown_tokens = []
+
+    print(get_freq_dict(lat_tokens))
+    print(get_freq_dict(de_tokens))
+    print(get_freq_dict(eng_tokens))
+
+    labels = ['eng', 'de', 'lat']
     corpus = [eng_tokens, de_tokens, lat_tokens]
     language_profiles = get_language_profiles(corpus, labels)
 
+    list_of_unique_words = get_language_features(language_profiles)
+
     known_text_vectors = []
+    unknown_text_vectors = []
     language_labels = []
     for text in DE_SAMPLES:
         tokens = remove_stop_words(tokenize(text), [])
@@ -62,15 +73,33 @@ if __name__ == '__main__':
         tokens = remove_stop_words(tokenize(text), [])
         known_text_vectors.append(get_text_vector(tokens, language_profiles))
         language_labels.append('lat')
+    for token in unknown_tokens:
+        unknown_text_vectors.append(get_text_vector(token, language_profiles))
+
+    minimal_vectors = len(min(known_text_vectors, key=len))
+    new_vectors = []
+    for vector in known_text_vectors:
+        new_vectors.append(vector[:minimal_vectors])
     unknown_text_vectors = []
     for text in UNKNOWN_SAMPLES:
         tokens = remove_stop_words(tokenize(text), [])
         unknown_text_vectors.append(get_text_vector(tokens, language_profiles))
-    result = []
+    new_unknown_text_vectors = []
     for vector in unknown_text_vectors:
-        result.append(predict_language_knn(vector, known_text_vectors, language_labels, 3)[0])
-        print(result)
-        print(EXPECTED)
+        new_unknown_text_vectors.append(vector[:minimal_vectors])
+
+    distance = calculate_distance(new_unknown_text_vectors[0], new_vectors[0])
+
+    distance_manhattan = calculate_distance_manhattan(new_unknown_text_vectors[0], new_vectors[0])
+
+    predict_language_score = predict_language_score(new_unknown_text_vectors[0], new_vectors, language_labels)
+    print(predict_language_score)
+
+    RESULT = []
+    for vector in new_unknown_text_vectors:
+        RESULT.append(predict_language_knn(vector, new_vectors, language_labels, 3)[0])
+    print(RESULT)
+    print(EXPECTED)
 
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT, 'Detection not working'
+    assert RESULT, EXPECTED
