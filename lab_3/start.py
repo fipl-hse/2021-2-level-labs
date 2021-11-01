357abbaebc508bc7f34795f9612a1cf3eaa2cb94
Dmitry Uraev <uraev.dima@yandex.ru>
2021-11-01 11:10:55 +0300

    update

--- lab_3/start.py
+++ lab_3/start.py
@@ -12,15 +12,16 @@ if __name__ == '__main__':
     UNKNOWN_SAMPLE = "Helium is material."
 
     # score 6, params: k = 5, trie_level = 2
+    # print(calculate_distance(unknown_profile, en_profile, 5, 2))
+    # print(calculate_distance(unknown_profile, de_profile, 5, 2))
     EXPECTED_DISTANCE_TO_EN_DE_PROFILES = 17, 25
 
     # score 8, k = 5, trie_level = 3
-    # print(detector.detect_scores(profile_unk, 5, 3))
+    # print(detector.detect(profile_unk, 5, 3))
     # EXPECTED_SCORE = {'en': 24, 'de': 25}
 
     # score 10, k = 1000, trie_levels = (2,)
     # print(detector.detect(unknown_profile, 1000, 2))
-    # print(detector.detect_scores(unknown_profile, 1000, 2))
     # text = """ Некој е болен и тој не е слободен. Dлетува гол во дупка од мраз.
     #     И пее, а плаче од болка. Дали е ова контраст, можеби – живот?"""
     # EXPECTED_LANGUAGE = ?
