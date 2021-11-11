920d28834860bdee6241d7f35cdcc3903057cc47
Artem Samoylovsky <90442858+samoylovsky@users.noreply.github.com>
2021-11-11 22:15:02 +0300

    Update main.py

--- lab_2/main.py
+++ lab_2/main.py
@@ -162,10 +162,10 @@ def predict_language_knn(unknown_text_vector: list, known_text_vectors: list,
     :param k: the number of neighbors to choose label from
     :param metric: specific metric to use while calculating distance
     """
-   if not isinstance(unknown_text_vector, list)\
-   or not isinstance(known_text_vectors, list)\
-   or not isinstance(language_labels, list)\
-   or not isinstance(k, int) or not isinstance(metric, str):
+    if not isinstance(unknown_text_vector, list)\ 
+    or not isinstance(known_text_vectors, list)\
+    or not isinstance(language_labels, list)\
+    or not isinstance(k, int) or not isinstance(metric, str):
         return None
     if len(language_labels) != len(known_text_vectors):
         return None
