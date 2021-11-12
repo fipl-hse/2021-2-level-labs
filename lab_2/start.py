0071f875f150b8aeae9985f434629b2b515b9a41
eapareshina <zhenya.pareshina@gmail.com>
2021-11-12 09:55:36 +0300

    a

--- lab_2/start.py
+++ lab_2/start.py
@@ -3,9 +3,8 @@ Language detection starter
 """
 
 import os
-from lab_2.main import (tokenize, remove_stop_words,
-                        get_language_profiles, get_sparse_vector,
-                        predict_language_knn_sparse)
+from lab_2.main import *
+
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_PROFILES_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'profiles')
@@ -64,6 +63,15 @@ if __name__ == '__main__':
         unknown_text_vector = get_sparse_vector(unknown_text, language_profiles)
         predicted_lang = predict_language_knn_sparse(unknown_text_vector, known_text_vectors,
                                                      language_labels, k)
+        RESULT.append(predicted_lang[0])
+    for text in corpus:
+        known_text_vectors.append(get_text_vector(text, language_profiles))
+    k = 3
+    RESULT = []
+    for text in UNKNOWN_SAMPLES:
+        unknown_text = remove_stop_words(tokenize(text), stop_words)
+        unknown_text_vector = get_text_vector(unknown_text, language_profiles)
+        predicted_lang = predict_language_knn(unknown_text_vector, known_text_vectors, language_labels, k)
         RESULT.append(predicted_lang[0])
     print(f"{RESULT} are possible languages")
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
