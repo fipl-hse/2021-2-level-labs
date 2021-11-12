a96c90739ffa2bc8752dd6331cc1a7c2d7923bec
titovakaterina <titova.katerina2017@yandex.ru>
2021-11-12 08:34:13 +0300

    les_2

--- lab_2/start.py
+++ lab_2/start.py
@@ -1,40 +1,9 @@
-"""
-Language detection starter
-"""
-
 import os
 from main import tokenize, remove_stop_words
-from lab_2.main import get_language_profiles, get_sparse_vector,\
-        predict_language_knn_sparse
+from lab_2.main import get_language_profiles
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_PROFILES_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'profiles')
-PATH_TO_DATASET_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'dataset')
-
-if __name__ == '__main__':
-    with open(os.path.join(PATH_TO_PROFILES_FOLDER, 'eng.txt'),
-              'r', encoding='utf-8') as file_to_read:
-        EN_TEXT = file_to_read.read()
-
-    with open(os.path.join(PATH_TO_PROFILES_FOLDER, 'de.txt'),
-              'r', encoding='utf-8') as file_to_read:
-        DE_TEXT = file_to_read.read()
-
-    with open(os.path.join(PATH_TO_PROFILES_FOLDER, 'lat.txt'),
-              'r', encoding='utf-8') as file_to_read:
-        LAT_TEXT = file_to_read.read()
-
-    with open(os.path.join(PATH_TO_DATASET_FOLDER, 'known_samples_de.txt'),
-              'r', encoding='utf-8') as file_to_read:
-        DE_SAMPLES = file_to_read.read().split('[TEXT]')[1:]
-
-    with open(os.path.join(PATH_TO_DATASET_FOLDER, 'known_samples_eng.txt'),
-              'r', encoding='utf-8') as file_to_read:
-        EN_SAMPLES = file_to_read.read().split('[TEXT]')[1:]
-
-    with open(os.path.join(PATH_TO_DATASET_FOLDER, 'known_samples_lat.txt'),
-              'r', encoding='utf-8') as file_to_read:
-        LAT_SAMPLES = file_to_read.read().split('[TEXT]')[1:]
 
     with open(os.path.join(PATH_TO_DATASET_FOLDER, 'unknown_samples.txt'),
               'r', encoding='utf-8') as file_to_read:
@@ -61,11 +30,15 @@ if __name__ == '__main__':
     RESULT = []
     for text in UNKNOWN_SAMPLES:
         unknown_text = remove_stop_words(tokenize(text), stop_words)
-        unknown_text_vector = get_sparse_vector(unknown_text, language_profiles)
-        predict_language = predict_language_knn_sparse(unknown_text_vector, known_text_vectors,
+        unknown_text_vector = get_vector(unknown_text, language_profiles)
+        predict_language = predict_language_knn(unknown_text_vector, known_text_vectors,
                                                        language_labels, k)
 
         RESULT.append(predict_language[0])
 
     print(RESULT)
     EXPECTED = ['de', 'eng', 'lat']
+    RESULT = ''
+
+    # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
+    assert RESULT, 'Detection not working'
