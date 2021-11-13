53928e1e916d69570e1bbbd9014ed2a35028ff0c
123 <a123@MacBook-Pro-123.local>
2021-11-13 16:36:00 +0300

    second change

--- lab_3/main.py
+++ lab_3/main.py
@@ -20,6 +20,8 @@ def tokenize_by_sentence(text: str) -> tuple:
          (('_', 'h', 'e', '_'), ('_', 'i', 's', '_'), ('_', 'h', 'a', 'p', 'p', 'y', '_'))
          )
     """
+    if not isinstance(text, str):
+        return None
     return text
 
 
