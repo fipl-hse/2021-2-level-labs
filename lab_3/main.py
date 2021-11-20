be3daeb241a37216c799877b9dfc612e27ca6656
lenazagg <lenazagovorova@gmail.com>
2021-11-20 17:26:44 +0300

    changes

--- lab_3/main.py
+++ lab_3/main.py
@@ -38,11 +38,10 @@ def tokenize_by_sentence(text: str) -> tuple:
                 tokenized_sentence += de_letters[symbol]
             elif symbol.isspace() or symbol.isalpha():
                 tokenized_sentence += symbol
-        tokenized_sentences.append(tokenized_sentence.split())
         tokenized_sentence = ''
 
     for x, tokenized_sentence in enumerate(tokenized_sentences):
-        tokenized_sentences[x] = tokenized_sentence
+        tokenized_sentences[x] = tokenized_sentence.split()
 
     for tokenized_sentence in tokenized_sentences:
         for y, word in enumerate(tokenized_sentence):
