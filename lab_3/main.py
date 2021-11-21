14db7da78ecdae21a63615d622b5db3622729985
KarinaZakirova <90447262+KarinaZakirova@users.noreply.github.com>
2021-11-21 19:39:45 +0300

    Something else 3.1 for code style

--- lab_3/main.py
+++ lab_3/main.py
@@ -267,10 +267,12 @@ class LanguageProfile:
         encoded_corpus = (((1, 2, 3, 1), (1, 4, 5, 1), (1, 2, 6, 7, 7, 8, 1)),)
         ngram_sizes = (2, 3)
 
-        self.tries --> [<__main__.NGramTrie object at 0x09DB9BB0>, <__main__.NGramTrie object at 0x09DB9A48>]
+        self.tries --> [<__main__.NGramTrie object at 0x09DB9BB0>,
+        <__main__.NGramTrie object at 0x09DB9A48>]
         self.n_words --> [11, 9]
         self.tries[0].n_grams --> (
-            (((1, 2), (2, 3), (3, 1)), ((1, 4), (4, 5), (5, 1)), ((1, 2), (2, 6), (6, 7), (7, 7), (7, 8), (8, 1))),
+            (((1, 2), (2, 3), (3, 1)), ((1, 4), (4, 5), (5, 1)), ((1, 2), (2, 6), (6, 7), (7, 7),
+            (7, 8), (8, 1))),
         )
         """
         if not (isinstance(encoded_corpus, tuple) and isinstance(ngram_sizes, tuple)):
@@ -283,17 +285,6 @@ class LanguageProfile:
                 return 1
             self.tries.append(n_gram_trie)
             self.n_words.append(len(n_gram_trie.n_gram_frequencies))
-        ''' 
-        for ngram_size in ngram_sizes:
-            count = 0
-            for n_gram_trie in self.tries:
-                for text in n_gram_trie.n_grams:
-                    for sentence in text:
-                        for n_gram in sentence:
-                            if len(n_gram) == ngram_size:
-                                count += 1
-            self.n_words.append(count)
-        '''
         return 0
 
     def get_top_k_n_grams(self, k: int, trie_level: int) -> tuple:
@@ -406,7 +397,8 @@ def calculate_distance(unknown_profile: LanguageProfile, known_profile: Language
     :param k: number of frequent N-grams to take into consideration
     :param trie_level: N-gram sizes to use in comparison
     :return: a distance
-    Например, первый набор N-грамм для неизвестного профиля - first_n_grams = ((1, 2), (4, 5), (2, 3)),
+    Например, первый набор N-грамм для неизвестного профиля - first_n_grams = ((1, 2), (4, 5),
+    (2, 3)),
     второй набор N-грамм для известного профиля – second_n_grams = ((1, 2), (2, 3), (4, 5)).
     Расстояние для (1, 2) равно 0, так как индекс в первом наборе – 0, во втором – 0, |0 – 0| = 0.
     Расстояние для (4, 5) равно 1, расстояние для (2, 3) равно 1.
@@ -459,7 +451,8 @@ class LanguageDetector:
         :param unknown_profile: a dictionary
         :param k: a number of the most common n-grams
         :param trie_levels: N-gram size - tuple with one int for score 8
-        :return: a dictionary with language labels and their scores if input is correct, otherwise -1
+        :return: a dictionary with language labels and their scores if input is correct, otherwise
+        -1
         """
         if not (isinstance(unknown_profile, LanguageProfile)
                 and isinstance(k, int)
@@ -493,8 +486,8 @@ class ProbabilityLanguageDetector(LanguageDetector):
     Detects profile language using probabilities
     """
 
-    def detect(self, unknown_profile: LanguageProfile, k: int, trie_levels: tuple) -> Dict[Tuple[
-                                                                                               str, int], int or float] or int:
+    def detect(self, unknown_profile: LanguageProfile, k: int, trie_levels: tuple) -> \
+            Dict[Tuple[str, int], int or float] or int:
         """
         Detects the language of an unknown profile and its probability score
         :param unknown_profile: an instance of LanguageDetector
