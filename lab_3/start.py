00f84c3406bdd49c5fbf25cff37df846cf72f0ca
VSvir <VSslava612@yandex.ru>
2021-11-22 18:47:51 +0300

    Add realisation of start.py module for score 6

--- lab_3/start.py
+++ lab_3/start.py
@@ -3,6 +3,7 @@ Language detection starter
 """
 
 import os
+import lab_3.main
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
@@ -19,6 +20,33 @@ if __name__ == '__main__':
     # print(calculate_distance(unknown_profile, de_profile, 5, 2))
     EXPECTED_DISTANCE_TO_EN_DE_PROFILES = 17, 25
 
+    TOKENIZED_ENG_SAMPLE = lab_3.main.tokenize_by_sentence(ENG_SAMPLE)
+    TOKENIZED_DE_SAMPLE = lab_3.main.tokenize_by_sentence(GERMAN_SAMPLE)
+    TOKENIZED_UNKNOWN_SAMPLE = lab_3.main.tokenize_by_sentence(UNKNOWN_SAMPLE)
+
+    STORAGE = lab_3.main.LetterStorage()
+    STORAGE.update(TOKENIZED_ENG_SAMPLE)
+    STORAGE.update(TOKENIZED_DE_SAMPLE)
+    STORAGE.update(TOKENIZED_UNKNOWN_SAMPLE)
+
+    ENCODED_ENG = lab_3.main.encode_corpus(STORAGE, TOKENIZED_ENG_SAMPLE)
+    ENCODED_DE = lab_3.main.encode_corpus(STORAGE, TOKENIZED_DE_SAMPLE)
+    ENCODED_UNKNOWN = lab_3.main.encode_corpus(STORAGE, TOKENIZED_UNKNOWN_SAMPLE)
+
+    PROFILE_ENG = lab_3.main.LanguageProfile(STORAGE, 'en')
+    PROFILE_DE = lab_3.main.LanguageProfile(STORAGE, 'de')
+    PROFILE_UNKNOWN = lab_3.main.LanguageProfile(STORAGE, 'unk')
+
+    PROFILE_ENG.create_from_tokens(ENCODED_ENG, (2,))
+    PROFILE_DE.create_from_tokens(ENCODED_DE, (2,))
+    PROFILE_UNKNOWN.create_from_tokens(ENCODED_UNKNOWN, (2,))
+
+    DISTANCE_ENG_UNK = lab_3.main.calculate_distance(PROFILE_UNKNOWN, PROFILE_ENG, 5, 2)
+    DISTANCE_DE_UNK = lab_3.main.calculate_distance(PROFILE_UNKNOWN, PROFILE_DE, 5, 2)
+
+    # print(DISTANCE_ENG_UNK)
+    # print(DISTANCE_DE_UNK)
+
     # score 8, k = 5, trie_level = 3
     # predict UNKNOWN_SAMPLE
     # print(detector.detect(profile_unk, 5, 3))
@@ -30,7 +58,7 @@ if __name__ == '__main__':
     # EXPECTED_LANGUAGE = ?
     # EXPECTED_MIN_DISTANCE = ?
 
-    RESULT = ''
+    RESULT = DISTANCE_ENG_UNK, DISTANCE_DE_UNK
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT, 'Detection not working'
+    assert EXPECTED_DISTANCE_TO_EN_DE_PROFILES == RESULT, 'Detection not working'
 
