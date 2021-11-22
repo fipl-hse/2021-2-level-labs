834240868052d5e2a7838196b36c3986adac6262
esgolovleva <golovleva.katerina@mail.ru>
2021-11-23 00:45:57 +0300

    start.py

--- lab_3/start.py
+++ lab_3/start.py
@@ -3,6 +3,15 @@ Language detection starter
 """
 
 import os
+from lab_3.main import \
+    tokenize_by_sentence, \
+    LetterStorage, \
+    encode_corpus, \
+    decode_corpus, \
+    NGramTrie, \
+    LanguageProfile, \
+    calculate_distance, \
+    LanguageDetector
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
@@ -31,6 +40,41 @@ if __name__ == '__main__':
     # print(detector.detect(unknown_profile, 1000, (2,)))
     # EXPECTED_LANGUAGE = ?
     # EXPECTED_MIN_DISTANCE = ?
+
+    # tokenize by sentence:
+    eng_tokens = tokenize_by_sentence(ENG_SAMPLE)
+    de_tokens = tokenize_by_sentence(GERMAN_SAMPLE)
+    unknown_tokens = tokenize_by_sentence(UNKNOWN_SAMPLE)
+
+    # creating letter storages
+    storage = LetterStorage()  # создание пустого экземпляра класса
+    storage.update(eng_tokens)
+    storage.update(de_tokens)
+    storage.update(unknown_tokens)
+
+    # encoding text
+    eng_text_encoded = encode_corpus(storage, eng_tokens)
+    de_text_encoded = encode_corpus(storage, de_tokens)
+    unk_text_encoded = encode_corpus(storage, unknown_tokens)
+
+    # decoding text
+    eng_text_decoded = decode_corpus(storage, eng_tokens)
+    de_text_decoded = decode_corpus(storage, de_tokens)
+    unk_text_decoded = decode_corpus(storage, unknown_tokens)
+
+    # creating n-grams
+    eng_grams = NGramTrie(3, storage)
+    de_grams = NGramTrie(3, storage)
+    unknown_grams = NGramTrie(3, storage)
+
+    # creating language profiles
+    eng_profile = LanguageProfile(storage, 'en')
+    de_profile = LanguageProfile(storage, 'de')
+    unknown_profile = LanguageProfile(storage, 'unknown')
+
+    # saving
+    # calculating distance
+    # detecting language
 
     RESULT = ''
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
