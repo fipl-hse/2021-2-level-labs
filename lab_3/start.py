d9dc8ec39aa65a0ec6afb68c9e85046886286fdd
Anastasia Matuschak <anastasia.mt12@gmail.com>
2021-11-23 01:22:49 +0300

    startpy

--- lab_3/start.py
+++ lab_3/start.py
@@ -3,6 +3,7 @@ Language detection starter
 """
 
 import os
+import lab_3.main
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
@@ -19,6 +20,33 @@ if __name__ == '__main__':
     # print(calculate_distance(unknown_profile, de_profile, 5, 2))
     EXPECTED_DISTANCE_TO_EN_DE_PROFILES = 17, 25
 
+    tokenize_unknown_sample = lab_3.main.tokenize_by_sentence(UNKNOWN_SAMPLE)
+    tokenize_eng_sample = lab_3.main.tokenize_by_sentence(ENG_SAMPLE)
+    tokenize_de_sample = lab_3.main.tokenize_by_sentence(GERMAN_SAMPLE)
+
+    storage = lab_3.main.LetterStorage()
+    storage.update(tokenize_unknown_sample)
+    storage.update(tokenize_eng_sample)
+    storage.update(tokenize_de_sample)
+
+    unk_enc = lab_3.main.encode_corpus(storage, tokenize_unknown_sample)
+    eng_enc = lab_3.main.encode_corpus(storage, tokenize_eng_sample)
+    de_enc = lab_3.main.encode_corpus(storage, tokenize_de_sample)
+
+    unknown_profile = lab_3.main.LanguageProfile(storage, 'unk')
+    eng_profile = lab_3.main.LanguageProfile(storage, 'en')
+    de_profile = lab_3.main.LanguageProfile(storage, 'de')
+
+    unknown_profile.create_from_tokens(unk_enc, (2,))
+    eng_profile.create_from_tokens(eng_enc, (2,))
+    de_profile.create_from_tokens(de_enc, (2,))
+
+    distance_unk_eng = lab_3.main.calculate_distance(unknown_profile, eng_profile, 5, 2)
+    distance_unk_de = lab_3.main.calculate_distance(unknown_profile, de_profile, 5, 2)
+
+    print(distance_unk_eng)
+    print(distance_unk_de)
+
     # score 8, k = 5, trie_level = 3
     # predict UNKNOWN_SAMPLE
     # print(detector.detect(profile_unk, 5, 3))
@@ -30,7 +58,7 @@ if __name__ == '__main__':
     # EXPECTED_LANGUAGE = ?
     # EXPECTED_MIN_DISTANCE = ?
 
-    RESULT = ''
+    RESULT = distance_unk_eng, distance_unk_de
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
 
