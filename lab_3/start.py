dd63cd4f35b15dcfb295a121962c51b5ed853a5e
NovIrina <90452047+NovIrina@users.noreply.github.com>
2021-11-23 15:52:55 +0300

    changes_1

--- lab_3/start.py
+++ lab_3/start.py
@@ -3,6 +3,8 @@ Language detection starter
 """
 
 import os
+from lab_3.main import LetterStorage, LanguageProfile, LanguageDetector, \
+    tokenize_by_sentence, encode_corpus, calculate_distance
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
@@ -12,17 +14,85 @@ if __name__ == '__main__':
     UNKNOWN_SAMPLE = "Helium is material."
     SECRET_SAMPLE = """ Некој е болен и тој не е слободен. Dлетува гол во дупка од мраз. 
     И пее, а плаче од болка. Дали е ова контраст, можеби – живот?"""
-
-    # score 6, params: k = 5, trie_level = 2
-    # predict UNKNOWN_SAMPLE
-    # print(calculate_distance(unknown_profile, en_profile, 5, 2))
-    # print(calculate_distance(unknown_profile, de_profile, 5, 2))
     EXPECTED_DISTANCE_TO_EN_DE_PROFILES = 17, 25
 
-    # score 8, k = 5, trie_level = 3
-    # predict UNKNOWN_SAMPLE
-    # print(detector.detect(profile_unk, 5, 3))
-    # EXPECTED_SCORE = {'en': 24, 'de': 25}
+
+    def get_6_score():
+        # score 6, params: k = 5, trie_level = 2
+        # predict UNKNOWN_SAMPLE
+        # print(calculate_distance(unknown_profile, en_profile, 5, 2))
+        # print(calculate_distance(unknown_profile, de_profile, 5, 2))
+        eng_text = tokenize_by_sentence(ENG_SAMPLE)
+        de_text = tokenize_by_sentence(GERMAN_SAMPLE)
+        unknown_text = tokenize_by_sentence(UNKNOWN_SAMPLE)
+
+        storage = LetterStorage()
+        storage.update(eng_text)
+        storage.update(de_text)
+        storage.update(unknown_text)
+
+        encoded_eng_text = encode_corpus(storage, eng_text)
+        encoded_de_text = encode_corpus(storage, de_text)
+        encoded_unknown_text = encode_corpus(storage, unknown_text)
+
+        profile_en = LanguageProfile(letter_storage=storage, language_name='en')
+        profile_en.create_from_tokens(encoded_eng_text, (2,))
+
+        profile_de = LanguageProfile(letter_storage=storage, language_name='de')
+        profile_de.create_from_tokens(encoded_de_text, (2,))
+
+        unknown_profile = LanguageProfile(letter_storage=storage, language_name='unk')
+        unknown_profile.create_from_tokens(encoded_unknown_text, (2,))
+
+        print(calculate_distance(unknown_profile, profile_en, 5, 2))
+        print(calculate_distance(unknown_profile, profile_de, 5, 2))
+
+
+    get_6_score()
+
+
+    def get_8_score():
+        # score 8, k = 5, trie_level = 3
+        # predict UNKNOWN_SAMPLE
+        # print(detector.detect(profile_unk, 5, 3))
+        # EXPECTED_SCORE = {'en': 24, 'de': 25}
+
+        eng_text = tokenize_by_sentence(ENG_SAMPLE)
+        de_text = tokenize_by_sentence(GERMAN_SAMPLE)
+        unknown_text = tokenize_by_sentence(UNKNOWN_SAMPLE)
+
+        storage = LetterStorage()
+        storage.update(eng_text)
+        storage.update(de_text)
+        storage.update(unknown_text)
+
+        encoded_eng_text = encode_corpus(storage, eng_text)
+        encoded_de_text = encode_corpus(storage, de_text)
+        encoded_unknown_text = encode_corpus(storage, unknown_text)
+
+        profile_en = LanguageProfile(letter_storage=storage, language_name='en')
+        profile_en.create_from_tokens(encoded_eng_text, (3,))
+
+        profile_de = LanguageProfile(letter_storage=storage, language_name='de')
+        profile_de.create_from_tokens(encoded_de_text, (3,))
+
+        unknown_profile_1 = LanguageProfile(letter_storage=storage, language_name='unk')
+        unknown_profile_1.create_from_tokens(encoded_unknown_text, (3,))
+
+        unknown_profile_1.save('unknown_profile.json')
+
+        unknown_profile_2 = LanguageProfile(letter_storage=storage, language_name='unk')
+        unknown_profile_2.open('unknown_profile.json')
+
+        detector = LanguageDetector()
+        detector.register_language(profile_en)
+        detector.register_language(profile_de)
+        print(detector.detect(unknown_profile_2, 5, (3,)))
+
+
+    get_8_score()
+
+
 
     # score 10, k = 1000, trie_levels = (2,)
     # predict SECRET_SAMPLE
@@ -33,4 +103,3 @@ if __name__ == '__main__':
     RESULT = ''
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
-
