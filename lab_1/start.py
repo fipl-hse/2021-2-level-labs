9df5a10a1379d7ea1c06074c1065455948174df8
eskondrashova <90447381+eskondrashova@users.noreply.github.com>
2021-11-23 23:15:20 +0300

    Update start.py

--- lab_1/start.py
+++ lab_1/start.py
@@ -11,14 +11,14 @@ PATH_TO_PROFILES_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'profiles')
 
 if __name__ == '__main__':
 
-    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'en.txt'), 'r', encoding='utf-8') as file_to_read_en:
-        en_text = file_to_read_en.read()
+    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'en.txt'), 'r', encoding='utf-8') as ftr_en:
+        en_text = ftr_en.read()
 
-    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'de.txt'), 'r', encoding='utf-8') as file_to_read_de:
-        de_text = file_to_read_de.read()
+    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'de.txt'), 'r', encoding='utf-8') as ftr_de:
+        de_text = ftr_de.read()
 
-    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'la.txt'), 'r', encoding='utf-8') as file_to_read_la:
-        la_text = file_to_read_la.read()
+    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'la.txt'), 'r', encoding='utf-8') as ftr_la:
+        la_text = ftr_la.read()
 
     with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'unknown.txt'), 'r', encoding='utf-8') as \
             file_to_read_unk:
