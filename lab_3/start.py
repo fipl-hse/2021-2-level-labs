54fc2972864765f4867091a44e98b71d04399bcf
Vlados13iq <znaminvlad@gmail.com>
2021-11-24 21:30:24 +0300

    i do not understand why do i get an error

--- lab_3/start.py
+++ lab_3/start.py
@@ -3,6 +3,11 @@ Language detection starter
 """
 
 import os
+from lab_3.main import \
+    tokenize_by_sentence, \
+    LetterStorage, \
+    encode_corpus, \
+    decode_corpus
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
@@ -13,6 +18,8 @@ if __name__ == '__main__':
     SECRET_SAMPLE = """ Некој е болен и тој не е слободен. Dлетува гол во дупка од мраз. 
     И пее, а плаче од болка. Дали е ова контраст, можеби – живот?"""
 
+
+
     # score 6, params: k = 5, trie_level = 2
     # predict UNKNOWN_SAMPLE
     # print(calculate_distance(unknown_profile, en_profile, 5, 2))
@@ -22,13 +29,30 @@ if __name__ == '__main__':
     # score 8, k = 5, trie_level = 3
     # predict UNKNOWN_SAMPLE
     # print(detector.detect(profile_unk, 5, 3))
-    # EXPECTED_SCORE = {'en': 24, 'de': 25}
+    #EXPECTED_SCORE = {'en': 24, 'de': 25}
 
     # score 10, k = 1000, trie_levels = (2,)
     # predict SECRET_SAMPLE
     # print(detector.detect(unknown_profile, 1000, (2,)))
     # EXPECTED_LANGUAGE = ?
     # EXPECTED_MIN_DISTANCE = ?
+
+    eng_tokenize = tokenize_by_sentence(ENG_SAMPLE)
+    german_tokenize = tokenize_by_sentence(GERMAN_SAMPLE)
+    unknown_tokenize = tokenize_by_sentence(UNKNOWN_SAMPLE)
+
+    storage = LetterStorage()
+    storage.update(eng_tokenize)
+    storage.update(german_tokenize)
+    storage.update(unknown_tokenize)
+
+    eng_tokenize_encoded = encode_corpus(storage, eng_tokenize)
+    german_tokenize_encoded = encode_corpus(storage, german_tokenize)
+    unknown_tokenize_encoded = encode_corpus(storage, unknown_tokenize)
+
+    eng_tokenize_decoded = decode_corpus(storage, eng_tokenize_encoded)
+    german_tokenize_decoded = decode_corpus(storage, german_tokenize_encoded)
+    unknown_tokenize_decoded = decode_corpus(storage, unknown_tokenize_encoded)
 
     RESULT = ''
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
