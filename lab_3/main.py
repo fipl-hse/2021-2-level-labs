86c25afec6001c3bde811126af3b05bef95bc9d5
lenazagg <lenazagovorova@gmail.com>
2021-11-24 22:26:11 +0300

    changes

--- lab_3/main.py
+++ lab_3/main.py
@@ -54,9 +54,6 @@ def tokenize_by_sentence(text: str) -> tuple:
     for x, tokenized_sentence in enumerate(tokenized_sentences):
         tokenized_sentences[x] = tokenized_sentence.split()
 
-    for x, tokenized_sentence in enumerate(tokenized_sentences):
-        tokenized_sentences[x] = tokenized_sentence.split()
-
     for tokenized_sentence in tokenized_sentences:
         for y, word in enumerate(tokenized_sentence):
             underscore.extend(word)
