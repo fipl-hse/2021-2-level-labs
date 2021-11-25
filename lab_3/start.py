d7dcfc39c2d9b28f8db18faa83904cb18394b2d7
lenazagg <lenazagovorova@gmail.com>
2021-11-25 13:25:36 +0300

    changes in start.py

--- lab_3/start.py
+++ lab_3/start.py
@@ -3,6 +3,7 @@ Language detection starter
 """
 
 import os
+from lab_3.main import tokenize_by_sentence, LetterStorage, encode_corpus
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
@@ -18,6 +19,22 @@ if __name__ == '__main__':
     # print(calculate_distance(unknown_profile, en_profile, 5, 2))
     # print(calculate_distance(unknown_profile, de_profile, 5, 2))
     EXPECTED_DISTANCE_TO_EN_DE_PROFILES = 17, 25
+
+    tokenized_un = tokenize_by_sentence(UNKNOWN_SAMPLE)
+    tokenized_en = tokenize_by_sentence(ENG_SAMPLE)
+    tokenized_ge = tokenize_by_sentence(GERMAN_SAMPLE)
+
+    un_letter_storage = LetterStorage()
+    un_letter_storage.update(tokenized_un)
+    encoded_un_corpus = encode_corpus(un_letter_storage, tokenized_un)
+
+    en_letter_storage = LetterStorage()
+    en_letter_storage.update(tokenized_en)
+    encoded_en_corpus = encode_corpus(en_letter_storage, tokenized_en)
+
+    ge_letter_storage = LetterStorage()
+    ge_letter_storage.update(tokenized_ge)
+    encoded_ge_corpus = encode_corpus(ge_letter_storage, tokenized_ge)
 
     # score 8, k = 5, trie_level = 3
     # predict UNKNOWN_SAMPLE
