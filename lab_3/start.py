901deea621d0d6a79c8eb32013eaf2a2e96ebc9b
ArturIvanov228 <90447341+ArturIvanov228@users.noreply.github.com>
2021-11-26 11:51:08 +0300

    Update start.py

--- lab_3/start.py
+++ lab_3/start.py
@@ -3,6 +3,7 @@ Language detection starter
 """
 
 import os
+from lab_3.main import tokenize_by_sentence, LetterStorage, encode_corpus, decode_corpus
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
@@ -13,24 +14,36 @@ if __name__ == '__main__':
     SECRET_SAMPLE = """ Некој е болен и тој не е слободен. Dлетува гол во дупка од мраз. 
     И пее, а плаче од болка. Дали е ова контраст, можеби – живот?"""
 
-    # score 6, params: k = 5, trie_level = 2
-    # predict UNKNOWN_SAMPLE
-    # print(calculate_distance(unknown_profile, en_profile, 5, 2))
-    # print(calculate_distance(unknown_profile, de_profile, 5, 2))
+
     EXPECTED_DISTANCE_TO_EN_DE_PROFILES = 17, 25
 
-    # score 8, k = 5, trie_level = 3
-    # predict UNKNOWN_SAMPLE
-    # print(detector.detect(profile_unk, 5, 3))
-    # EXPECTED_SCORE = {'en': 24, 'de': 25}
 
-    # score 10, k = 1000, trie_levels = (2,)
-    # predict SECRET_SAMPLE
-    # print(detector.detect(unknown_profile, 1000, (2,)))
-    # EXPECTED_LANGUAGE = ?
-    # EXPECTED_MIN_DISTANCE = ?
+    eng_tokenize = tokenize_by_sentence(ENG_SAMPLE)
+    german_tokenize = tokenize_by_sentence(GERMAN_SAMPLE)
+    unknown_tokenize = tokenize_by_sentence(UNKNOWN_SAMPLE)
 
-    RESULT = ''
+    print(eng_tokenize, german_tokenize, unknown_tokenize)
+
+    storage = LetterStorage()
+    storage.update(eng_tokenize)
+    storage.update(german_tokenize)
+    storage.update(unknown_tokenize)
+
+    eng_tokenize_encoded = encode_corpus(storage, eng_tokenize)
+    german_tokenize_encoded = encode_corpus(storage, german_tokenize)
+    unknown_tokenize_encoded = encode_corpus(storage, unknown_tokenize)
+
+    print(eng_tokenize_encoded, german_tokenize_encoded, unknown_tokenize_encoded)
+
+    eng_tokenize_decoded = decode_corpus(storage, eng_tokenize_encoded)
+    german_tokenize_decoded = decode_corpus(storage, german_tokenize_encoded)
+    unknown_tokenize_decoded = decode_corpus(storage, unknown_tokenize_encoded)
+
+    print(eng_tokenize_decoded, german_tokenize_decoded, unknown_tokenize_decoded)
+
+    RESULT = f'{eng_tokenize_decoded}, {german_tokenize_decoded}, {unknown_tokenize_decoded}'
+
+
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
 
