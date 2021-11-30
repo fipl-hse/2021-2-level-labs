b31f01714a3483e63487c740d765201a22f50db7
WhiteJaeger <logerk3@gmail.com>
2021-11-30 10:16:33 +0300

    sync with main repository

--- lab_1/start.py
+++ lab_1/start.py
@@ -11,16 +11,13 @@ PATH_TO_PROFILES_FOLDER = os.path.join(PATH_TO_LAB_FOLDER, 'profiles')
 
 if __name__ == '__main__':
 
-    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'en.txt'),
-              'r', encoding='utf-8') as file_to_read_en:
+    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'en.txt'), 'r', encoding='utf-8') as file_to_read_en:
         en_text = file_to_read_en.read()
 
-    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'de.txt'),
-              'r', encoding='utf-8') as file_to_read_de:
+    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'de.txt'), 'r', encoding='utf-8') as file_to_read_de:
         de_text = file_to_read_de.read()
 
-    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'la.txt'),
-              'r', encoding='utf-8') as file_to_read_la:
+    with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'la.txt'), 'r', encoding='utf-8') as file_to_read_la:
         la_text = file_to_read_la.read()
 
     with open(os.path.join(PATH_TO_TEXTS_FOLDER, 'unknown.txt'), 'r', encoding='utf-8') as \
