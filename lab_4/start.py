ed62f8687e9786a959bc37329d41fcca70b34c63
Anastasia Matuschak <anastasia.mt12@gmail.com>
2021-12-14 03:08:14 +0500

    voglio morire

--- lab_4/start.py
+++ lab_4/start.py
@@ -1,17 +1,51 @@
+"""
+Language generation starter
+"""
 
-from lab_4.main import tokenize_by_letters, LetterStorage
+import os
+from lab_4.main import tokenize_by_letters, LetterStorage, LanguageProfile, \
+    NGramTextGenerator, encode_corpus, decode_sentence, translate_sentence_to_plain_text
 
-with open('reference_text.txt', 'r', encoding='utf-8') as file_to_read:
-    reference_text = file_to_read.read()
 
-tokenized_text = tokenize_by_letters(reference_text)
-storage = LetterStorage()
-storage.update(tokenized_text)
+PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
-num_of_letters = storage.get_letter_count()
-lowest_id = list(storage.storage.keys())
-highest_id = list(storage.storage.keys())
+if __name__ == '__main__':
 
-print('The number of letters is:', num_of_letters)
-print('5 letters with the lowest id:', lowest_id[:5])
-print('5 letters with the highest id:', highest_id[-5:])
+    # find the appropriate start.py task in your lab_4 description file
+    # your code goes here
+
+    with open('reference_text.txt', 'r', encoding='utf-8') as file_to_read:
+        reference_text = file_to_read.read()
+
+    #score 4
+    tokenized_text = tokenize_by_letters(reference_text)
+    storage = LetterStorage()
+    storage.update(tokenized_text)
+
+    num_of_letters = storage.get_letter_count()
+    lowest_id = list(storage.storage.keys())
+    highest_id = list(storage.storage.keys())
+
+    print('The number of letters is:', num_of_letters)
+    print('5 letters with the lowest id:', lowest_id[:5])
+    print('5 letters with the highest id:', highest_id[-5:])
+
+    #score 6
+
+    encoded = encode_corpus(storage, tokenized_text)
+    profile = LanguageProfile(storage, 'en')
+    profile.create_from_tokens(encoded, (2,))
+
+    generator = NGramTextGenerator(profile)
+    generated_text = generator.generate_sentence((3,), 10)
+    decoded = decode_sentence(storage, generated_text)
+
+    RESULT_FOR_6 = translate_sentence_to_plain_text(decoded)
+    print(RESULT_FOR_6)
+    # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
+    assert RESULT_FOR_6, 'Detection not working'
+
+
+
+
+
