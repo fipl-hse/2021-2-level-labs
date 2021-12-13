dd3bb2756ca512084aae44a8e80243bbeb0c8e5d
tanynova99 <balabolka2000@gmail.com>
2021-12-13 22:57:04 +0300

    Score 6 raw start.py

--- lab_4/start.py
+++ lab_4/start.py
@@ -1,28 +1,55 @@
-import os
-from lab_4.main import tokenize_by_letters, LetterStorage
+"""
+Language generation starter
+"""
 
-"""
-Mark 4
-Токенизируйте текст, который хранится в файле reference_text.txt.
-Заполните хранилище класса LetterStorage буквами.
-Выведите количество букв в вашем хранилище.
-Выведите 5 букв с наименьшим идентификатором,
-а также 5 букв с наибольшим идентификатором.
-"""
+import os
+from lab_4.main import tokenize_by_letters, LetterStorage, LanguageProfile, \
+    NGramTextGenerator, decode_sentence, encode_corpus, \
+    translate_sentence_to_plain_text
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
-with open(os.path.join(PATH_TO_LAB_FOLDER, 'reference_text.txt'),
-          'r', encoding='utf-8') as file_to_read:
-    reference_text = file_to_read.read()
+if __name__ == '__main__':
 
-reference_text_tokens = tokenize_by_letters(reference_text)
-storage = LetterStorage()
-storage.update(reference_text_tokens)
-letters_id = list(storage.storage.keys())
+    # find the appropriate start.py task in your lab_4 description file
+    # your code goes here
 
-print("The № of letters:", storage.get_letter_count())
-print("Top-5 of the lowest id numbers:", letters_id[:5])
-print("Top-5 of the highest id numbers:", letters_id[-5:])
+    """
+    Mark 4
+    """
+
+    PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
+
+    with open(os.path.join(PATH_TO_LAB_FOLDER, 'reference_text.txt'),
+              'r', encoding='utf-8') as file_to_read:
+        REFERENCE_TEXT = file_to_read.read()
+
+    REFERENCE_TEXT_TOKENS = tokenize_by_letters(REFERENCE_TEXT)
+    storage = LetterStorage()
+    storage.update(REFERENCE_TEXT_TOKENS)
+    letters_id = list(storage.storage.keys())
+
+    print("The № of letters:", storage.get_letter_count())
+    print("Top-5 of the lowest id numbers:", letters_id[:5])
+    print("Top-5 of the highest id numbers:", letters_id[-5:])
+
+    """
+    Mark 6
+    """
+    encoded = encode_corpus(storage, REFERENCE_TEXT_TOKENS)
+    profile = LanguageProfile(storage, 'en')
+    profile.create_from_tokens(encoded, (2,))
+
+    generator = NGramTextGenerator(profile)
+    sentence = generator.generate_sentence((1,), 10)
+    decoded = decode_sentence(storage, sentence)
+
+    RESULT = translate_sentence_to_plain_text(decoded)
+    print('Generated sentence:', RESULT)
+
+    # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
+    assert RESULT, 'Detection not working'
+
+
 
 
