b911ba6b6b8f67813b2b6bbfab798e3646efd639
Vasilisa-Blyudova <90454966+Vasilisa-Blyudova@users.noreply.github.com>
2021-12-14 20:58:31 +0300

    start3 again

--- lab_3/start.py
+++ lab_3/start.py
@@ -129,72 +129,13 @@ if __name__ == '__main__':
     EXPECTED_MIN_DISTANCE = score_10()[1]
     print(EXPECTED_LANGUAGE, EXPECTED_MIN_DISTANCE)
 
-    def get_6_score():
-        """ score 6, params: k = 5, trie_level = 2
-            predict UNKNOWN_SAMPLE
-            print(calculate_distance(unknown_profile, en_profile, 5, 2))
-            print(calculate_distance(unknown_profile, de_profile, 5, 2))"""
-        english_text = tokenize_by_sentence(ENG_SAMPLE)
-        deutsch_text = tokenize_by_sentence(GERMAN_SAMPLE)
-        unknown_text = tokenize_by_sentence(UNKNOWN_SAMPLE)
-        letter_storage = LetterStorage()
-        letter_storage.update(english_text)
-        letter_storage.update(deutsch_text)
-        letter_storage.update(unknown_text)
-        encoded_english_text = encode_corpus(letter_storage, english_text)
-        encoded_deutsch_text = encode_corpus(letter_storage, deutsch_text)
-        encoded_unknown_text = encode_corpus(letter_storage, unknown_text)
-        profile_en = LanguageProfile(letter_storage=letter_storage, language_name='en')
-        profile_en.create_from_tokens(encoded_english_text, (2,))
-        profile_de = LanguageProfile(letter_storage=letter_storage, language_name='de')
-        profile_de.create_from_tokens(encoded_deutsch_text, (2,))
-        unknown_profile = LanguageProfile(letter_storage=letter_storage, language_name='unk')
-        unknown_profile.create_from_tokens(encoded_unknown_text, (2,))
-        distance_en = calculate_distance(unknown_profile, profile_en, 5, 2)
-        distance_de = calculate_distance(unknown_profile, profile_de, 5, 2)
-        print(distance_en, distance_de)
-
-    get_6_score()
-
-    EXPECTED = {'en': 24, 'de': 25}
-
-    def get_8_score():
-        """ score 8, k = 5, trie_level = 3
-            # predict UNKNOWN_SAMPLE
-            # print(detector.detect(profile_unk, 5, 3))
-            # EXPECTED_SCORE = {'en': 24, 'de': 25}"""
-        english_text = tokenize_by_sentence(ENG_SAMPLE)
-        deutsch_text = tokenize_by_sentence(GERMAN_SAMPLE)
-        unknown_text = tokenize_by_sentence(UNKNOWN_SAMPLE)
-        letter_storage = LetterStorage()
-        letter_storage.update(english_text)
-        letter_storage.update(deutsch_text)
-        letter_storage.update(unknown_text)
-        encoded_english_text = encode_corpus(letter_storage, english_text)
-        encoded_deutsch_text = encode_corpus(letter_storage, deutsch_text)
-        encoded_unknown_text = encode_corpus(letter_storage, unknown_text)
-        profile_en = LanguageProfile(letter_storage=letter_storage, language_name='en')
-        profile_en.create_from_tokens(encoded_english_text, (3,2))
-        profile_de = LanguageProfile(letter_storage=letter_storage, language_name='de')
-        profile_de.create_from_tokens(encoded_deutsch_text, (3,2))
-        unknown_profile_1 = LanguageProfile(letter_storage=letter_storage, language_name='unk')
-        unknown_profile_1.create_from_tokens(encoded_unknown_text, (3,2))
-        unknown_profile_1.save('unknown_profile.json')
-        unknown_profile_2 = LanguageProfile(letter_storage=letter_storage, language_name='unk')
-        unknown_profile_2.open('unknown_profile.json')
-        language_detector = LanguageDetector()
-        language_detector.register_language(profile_en)
-        language_detector.register_language(profile_de)
-        result = language_detector.detect(unknown_profile_2, 5, (3,))
-        return result
-
     # score 10, k = 1000, trie_levels = (2,)
     # predict SECRET_SAMPLE
     # print(detector.detect(unknown_profile, 1000, (2,)))
     # EXPECTED_LANGUAGE = ?
     # EXPECTED_MIN_DISTANCE = ?
 
-    RESULT = get_8_score()
+    RESULT = score_8()
     print(RESULT)
 
     # RESULT = ''
