1d9ec9e37c9ac3cf3816ff6a3ebb3ab8518aecf9
IrinaKurnakova <ikurnakova2002@mail.ru>
2021-12-14 21:51:27 +0300

    start.py

--- lab_4/start.py
+++ lab_4/start.py
@@ -3,10 +3,10 @@ Language generation starter
 """
 
 import os
+import random
 from lab_4.main import (tokenize_by_letters, LetterStorage,
                         LanguageProfile, NGramTextGenerator,
-                        decode_sentence, encode_corpus,
-                        translate_sentence_to_plain_text)
+                        encode_corpus, LikelihoodBasedTextGenerator)
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     storage = LetterStorage()
     storage.update(tokenized_text)
 
-    # score 4
+    # SCORE 4
     print('The number of letters in storage:', storage.get_letter_count())
     print('Top 5 letters with the lowest id:', list(storage.storage.keys())[:5])
     print('Top 5 letters with the highest id:', list(storage.storage.keys())[-5:])
@@ -30,11 +30,15 @@ if __name__ == '__main__':
     profile.create_from_tokens(encoded, (2,))
 
     text_generator = NGramTextGenerator(profile)
-    generated_sentence = text_generator.generate_sentence((3,), 5)
-    decoded_sentence = decode_sentence(storage, generated_sentence)
-    plain_text = translate_sentence_to_plain_text(decoded_sentence)
-    print('generated sentence:', plain_text)
+    generated_sentence = text_generator.generate_decoded_sentence((3,), 5)
+    print('Generated sentence:', generated_sentence)
 
-    RESULT = ''
+    # SCORE 8
+    text_generator_likelihood = LikelihoodBasedTextGenerator(profile)
+    for i in range(5):
+        print('Generated sentence (likelihood): ',
+              text_generator_likelihood.generate_decoded_sentence((7,), random.randint(5, 10)))
+
+    RESULT = generated_sentence
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
