a3503342d38faaf4b231857a8ec70b20bce487ab
VSvir <VSslava612@yandex.ru>
2021-12-14 20:26:54 +0300

    Add realisation of start.py module for score 6

--- lab_4/start.py
+++ lab_4/start.py
@@ -4,6 +4,7 @@ Language generation starter
 
 import os
 import main
+from lab_4.language_profile import LanguageProfile
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 PATH_TO_TEXT_FOLDER = os.path.join(PATH_TO_LAB_FOLDER)
@@ -27,6 +28,16 @@ if __name__ == '__main__':
     print('the number of letters is: ', ENG_STORAGE.get_letter_count())
     print('first five letters are: ', list(ENG_STORAGE.storage)[1:6])
     print('last five letters are: ', list(ENG_STORAGE.storage)[-5:])
+
+    # for score 6
+
+    ENCODED_CORPUS = main.encode_corpus(ENG_STORAGE, TOKENIZED_CORPUS)
+    PROFILE = LanguageProfile(ENG_STORAGE, 'en')
+    PROFILE.create_from_tokens(ENCODED_CORPUS, (2,))
+    TEXT_GENERATOR = main.NGramTextGenerator(PROFILE)
+    print(TEXT_GENERATOR.generate_decoded_sentence((1,), 5))
+    print(TEXT_GENERATOR.generate_decoded_sentence((1,), 6))
+    print(TEXT_GENERATOR.generate_decoded_sentence((1,), 7))
 
     RESULT = ''
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
