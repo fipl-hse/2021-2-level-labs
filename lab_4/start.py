648b880a0e3b19ebbdd62e0ee9abbaf99b064378
VSvir <VSslava612@yandex.ru>
2021-12-14 19:59:55 +0300

    Update start.py

--- lab_4/start.py
+++ lab_4/start.py
@@ -1,15 +1,33 @@
+"""
+Language generation starter
+"""
+
+import os
 import main
 
-with open('reference_text.txt', 'r', encoding='UTF-8') as read_text:
-    CORPUS = read_text.read()
+PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
+PATH_TO_TEXT_FOLDER = os.path.join(PATH_TO_LAB_FOLDER)
 
-# for score 4
+if __name__ == '__main__':
 
-TOKENIZED_CORPUS = main.tokenize_by_letters(CORPUS)
+    # find the appropriate start.py task in your lab_4 description file
+    # your code goes here
 
-ENG_STORAGE = main.LetterStorage()
-ENG_STORAGE.update(TOKENIZED_CORPUS)
+    with open(os.path.join(PATH_TO_TEXT_FOLDER, 'reference_text.txt'), 'r', encoding='utf-8') \
+            as read_text:
+        CORPUS = read_text.read()
 
-print('the number of letters is: ', ENG_STORAGE.get_letter_count())
-print('first five letters are: ', list(ENG_STORAGE.storage)[:5])
-print('last five letters are: ', list(ENG_STORAGE.storage)[-5:])
+    # for score 4
+
+    TOKENIZED_CORPUS = main.tokenize_by_letters(CORPUS)
+
+    ENG_STORAGE = main.LetterStorage()
+    ENG_STORAGE.update(TOKENIZED_CORPUS)
+
+    print('the number of letters is: ', ENG_STORAGE.get_letter_count())
+    print('first five letters are: ', list(ENG_STORAGE.storage)[1:6])
+    print('last five letters are: ', list(ENG_STORAGE.storage)[-5:])
+
+    RESULT = ''
+    # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
+    # assert RESULT, 'Detection not working'
