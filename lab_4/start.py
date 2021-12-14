286162d0d0fb81f1dfcaf3f75ba81328f15b76b9
Vlados13iq <znaminvlad@gmail.com>
2021-12-14 20:50:18 +0300

    i wrote code in start py

--- lab_4/start.py
+++ lab_4/start.py
@@ -3,6 +3,9 @@ Language generation starter
 """
 
 import os
+from lab_4.main import \
+    tokenize_by_letters, \
+    LetterStorage
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
@@ -11,6 +14,19 @@ if __name__ == '__main__':
     # find the appropriate start.py task in your lab_4 description file
     # your code goes here
 
-    RESULT = ''
+    with open(os.path.join(PATH_TO_LAB_FOLDER, 'reference_text.txt'), 'r', encoding='utf-8') as file:
+        text = file.read()
+
+    tokenized_text = tokenize_by_letters(text)
+    storage = LetterStorage()
+    storage.update(tokenized_text)
+
+    print('Number of letters: ', storage.get_letter_count())
+    print('Lowest ids: ', list(storage.storage.items())[:5])
+    print('Highest ids: ', list(storage.storage.items())[-5:])
+
+    RESULT = storage.get_letter_count(), \
+             list(storage.storage.items())[:5], \
+             list(storage.storage.items())[-5:]
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
