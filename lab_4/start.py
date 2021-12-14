99151f61b8f67fb55419bdc38ac1a51a9231b0f0
AnnaSizykh <aasizykh@edu.hse.ru>
2021-12-14 22:56:32 +0300

    changes in start.py

--- lab_4/start.py
+++ lab_4/start.py
@@ -13,36 +13,33 @@ if __name__ == '__main__':
 
     # find the appropriate start.py task in your lab_4 description file
     # your code goes here
-    with open(os.path.join(PATH_TO_LAB_FOLDER, 'reference_text.txt'), 'r', encoding='utf-8') as file_to_read:
-        text = file_to_read.read()
+    with open(os.path.join(PATH_TO_LAB_FOLDER, 'reference_text.txt'), 'r', encoding='utf-8') as f:
+        text = f.read()
+    tokenized_text = tokenize_by_letters(text)
+    storage = LetterStorage()
+    storage.update(tokenized_text)
 
-        #4
-        tokenized_text = tokenize_by_letters(text)
-        storage = LetterStorage()
-        storage.update(tokenized_text)
+    print(f'number of letters: {storage.get_letter_count()}')
 
-        print(f'number of letters: {storage.get_letter_count()}')
+    index_dict = sorted(storage.storage.items(), key=lambda x: x[1])
 
-        index_dict = sorted(storage.storage.items(), key=lambda x: x[1])
+    print(f'first 5: {index_dict[0:5]}')
+    print(f'last 5: {index_dict[-1:-6:-1]}')
 
-        print(f'first 5: {index_dict[0:5]}')
-        print(f'last 5: {index_dict[-1:-6:-1]}')
+    storage_2 = LetterStorage()
+    storage_2.update(tokenized_text)
+    profile = LanguageProfile(storage_2, 'en')
+    encoded_text_thing = encode_corpus(storage_2, tokenized_text)
+    profile.create_from_tokens(encoded_text_thing, (2,))
+    generator = NGramTextGenerator(profile)
+    sentence_1 = generator.generate_decoded_sentence((1,), 6)
+    sentence_2 = generator.generate_decoded_sentence((4,), 8)
+    sentence_3 = generator.generate_decoded_sentence((2,), 5)
 
-        #6
-        storage_2 = LetterStorage()
-        storage_2.update(tokenized_text)
-        profile = LanguageProfile(storage_2, 'en')
-        encoded_text_thing = encode_corpus(storage_2,tokenized_text)
-        profile.create_from_tokens(encoded_text_thing, (2,))
-        generator = NGramTextGenerator(profile)
-        sentence_1 = generator.generate_decoded_sentence((1,), 6)
-        sentence_2 = generator.generate_decoded_sentence((4,), 8)
-        sentence_3 = generator.generate_decoded_sentence((2,), 5)
+    print(sentence_1)
+    print(sentence_2)
+    print(sentence_3)
 
-        print(sentence_1)
-        print(sentence_2)
-        print(sentence_3)
-
-    RESULT = ''
-    # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
-    assert RESULT, 'Detection not working'
+RESULT = ''
+# DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
+assert RESULT, 'Detection not working'
