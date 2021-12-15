da008cb4df1b711398a949f8fc06afe6edf7e357
SophyaPerepelkina <saperepelkina@edu.hse.ru>
2021-12-15 22:04:19 +0300

    start py

--- lab_4/start.py
+++ lab_4/start.py
@@ -8,9 +8,20 @@ PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == '__main__':
 
-    # find the appropriate start.py task in your lab_4 description file
-    # your code goes here
+    with open(os.path.join(PATH_TO_LAB_FOLDER, 'reference_text.txt'), 'r', encoding='utf-8') as file:
+        text = file.read()
 
-    RESULT = ''
+    tokenized_text = tokenize_by_letters(text)
+    storage = LetterStorage()
+    storage.update(tokenized_text)
+
+    print('Number of letters: ', storage.get_letter_count())
+    print('Lowest ids: ', list(storage.storage.items())[:5])
+    print('Highest ids: ', list(storage.storage.items())[-5:])
+
+    RESULT = storage.get_letter_count(), \
+             list(storage.storage.items())[:5], \
+             list(storage.storage.items())[-5:]
+
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
