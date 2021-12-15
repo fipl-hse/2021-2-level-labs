83e2ac2b3233492d8e23a38f7b25373cda5dcd69
SophyaPerepelkina <saperepelkina@edu.hse.ru>
2021-12-15 22:07:58 +0300

    start py corr

--- lab_4/start.py
+++ lab_4/start.py
@@ -3,6 +3,9 @@ Language generation starter
 """
 
 import os
+from lab_4.main import \
+    tokenize_by_letters, \
+    LetterStorage
 
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
