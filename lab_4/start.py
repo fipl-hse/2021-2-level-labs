ff99ce7e03d32063f8e90f41c0096cd9b2b6cca4
esgolovleva <golovleva.katerina@mail.ru>
2021-12-15 17:07:23 +0300

    start.py

--- lab_4/start.py
+++ lab_4/start.py
@@ -16,12 +16,12 @@ if __name__ == '__main__':
 
     def score_4():
         with open(os.path.join(PATH_TO_LAB_FOLDER, 'reference_text.txt'), 'r', encoding="utf-8")\
-            as file:
+                as file:
             text = file.read()
         tokenized_text = tokenize_by_letters(text)
         storage = LetterStorage()
         storage.update(tokenized_text)
-        #RETURN
+
         number_of_letters = storage.get_letter_count()
         the_lowest_id = list(storage.storage.items())[:5]
         the_highest_id = list(storage.storage.items())[-5:]
@@ -32,17 +32,17 @@ if __name__ == '__main__':
 
     def score_6():
         with open(os.path.join(PATH_TO_LAB_FOLDER, 'reference_text.txt'), 'r', encoding="utf-8")\
-            as file:
+                as file:
             text = file.read()
         tokenized_text = tokenize_by_letters(text)
         storage = LetterStorage()
         storage.update(tokenized_text)
-        #ENCODE
+
         encoded_text = encode_corpus(storage, tokenized_text)
-        #LANGUAGE PROFILE
+
         profile = LanguageProfile(storage, 'en')
         profile.create_from_tokens(encoded_text, (2,))
-        #GENERATION
+
         generate_text = NGramTextGenerator(profile)
         sentences = []
         decoded_sentences = []
@@ -50,27 +50,27 @@ if __name__ == '__main__':
             sentences.append(generate_text.generate_decoded_sentence((1,), length))
             decoded_corpus = decode_sentence(storage, sentences)
             decoded_sentences.append(translate_sentence_to_plain_text(decoded_corpus))
-        #PRINT
+
         return decoded_sentences
 
     def score_8():
         with open(os.path.join(PATH_TO_LAB_FOLDER, 'reference_text.txt'), 'r', encoding="utf-8")\
-            as file:
+                as file:
             text = file.read()
         tokenized_text = tokenize_by_letters(text)
         storage = LetterStorage()
         storage.update(tokenized_text)
-        # ENCODE
+
         encoded_text = encode_corpus(storage, tokenized_text)
-        # LANGUAGE PROFILE
+
         profile = LanguageProfile(storage, 'en')
         profile.create_from_tokens(encoded_text, (2,))
-        # GENERATION
+
         generate_text = LikelihoodBasedTextGenerator(profile)
         sentences = []
         for length in range(5, 10):
             sentences.append(generate_text.generate_decoded_sentence((1,), length))
-        # PRINT
+
         return sentences
 
 
