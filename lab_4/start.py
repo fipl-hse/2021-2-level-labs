fd185be9e649e7ddcefd76dede24633f93d9efba
froschalad <vaaladyshkin@edu.hse.ru>
2021-12-16 16:44:24 +0300

    draft of start

--- lab_4/start.py
+++ lab_4/start.py
@@ -17,6 +17,17 @@ if __name__ == '__main__':
     # find the appropriate start.py task in your lab_4 description file
     # your code goes here
 
+    with open(os.path.join(PATH_TO_LAB_FOLDER, "reference_text.txt"), "r", encoding="utf-8") as file_to_read:
+        text = file_to_read.read()
+
+    tokenized_text = tokenize_by_letters(text)
+    storage = LetterStorage()
+    storage.update(tokenized_text)
+
+    number_of_letters = storage.get_letter_count()
+    low_id = list(storage.storage)[:5]
+    high_id = list(storage.storage)[-5:]
+
     RESULT = ''
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
