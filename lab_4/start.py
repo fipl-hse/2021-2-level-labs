aef4b12f4b1e2acd66c84e6c7bcbe1daaadf284e
DaryaKhodyreva <dashanasha2002@gmail.com>
2021-12-16 22:54:16 +0300

    start.py

--- lab_4/start.py
+++ lab_4/start.py
@@ -3,13 +3,15 @@ Language generation starter
 """
 
 import os
-from main import tokenize_by_letters, LetterStorage
+from main import tokenize_by_letters, LetterStorage, encode_corpus, LanguageProfile, NGramTextGenerator
 PATH_TO_LAB_FOLDER = os.path.dirname(os.path.abspath(__file__))
 
 if __name__ == '__main__':
 
     # find the appropriate start.py task in your lab_4 description file
     # your code goes here
+
+    # for 4 score
     with open(os.path.join(PATH_TO_LAB_FOLDER, 'reference_text.txt'), 'r', encoding='utf-8') as file:
         text = file.read()
     tokenization_txt = tokenize_by_letters(text)
@@ -24,5 +26,19 @@ if __name__ == '__main__':
              'Five letters with the least id: {}'.format(id_least), \
              'Five letters with the most id: {}'.format(id_most)
     print(RESULT)
+
+    # for 6 score
+    encoded_corpus = encode_corpus(text_storage, tokenization_txt)
+    en_profile = LanguageProfile(text_storage, 'en')
+    en_profile.create_from_tokens(encoded_corpus, (2,))
+
+    txt_generator = NGramTextGenerator(en_profile)
+
+    get_sentence_1 = txt_generator.generate_decoded_sentence((3,), 5)
+    get_sentence_2 = txt_generator.generate_decoded_sentence((3,), 6)
+    get_sentence_3 = txt_generator.generate_decoded_sentence((3,), 7)
+
+    print('Generated sentence are: {}; {}; {}'.format(get_sentence_1, get_sentence_2, get_sentence_3))
+
     # DO NOT REMOVE NEXT LINE - KEEP IT INTENTIONALLY LAST
     assert RESULT, 'Detection not working'
