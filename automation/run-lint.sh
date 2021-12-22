4244f03eb7ead8042e54a9d6fd8edab2f34dfb01
WhiteJaeger <logerk3@gmail.com>
2021-12-22 18:31:20 +0300

    add missing seminar title, fail if lint fail

--- automation/run-lint.sh
+++ automation/run-lint.sh
@@ -5,6 +5,7 @@ export PYTHONPATH="$(pwd)/lab_1:$(pwd)/lab_2:$(pwd)/lab_3:$(pwd)/lab_4:$(pwd):${
 echo -e '\n'
 echo 'Running lint check...'
 
+FAILED=0
 LABS=$(cat automation/labs.txt)
 
 for lab in $LABS; do
@@ -14,4 +15,14 @@ for lab in $LABS; do
 
 	lint_output=$(python3 -m pylint --rcfile=automation/.pylintrc lab_"${lab}")
   python3 automation/check_lint.py --lint-output "$lint_output" --target-score $TARGET_SCORE
+  if [[ $? -ne 0 ]]; then
+    FAILED=1
+  fi
 done
+
+if [[ ${FAILED} -eq 1 ]]; then
+	echo "Lint check failed."
+	exit 1
+fi
+
+echo "Lint check passed."
